04477b240c82fc9b6cad5e66f1392fbd213755c6
DanielRivasMD <danielrivasmd@gmail.com>
2020-02-02 15:49:05 +0100

    khal purge ics dups

---
+++ khal/purge_super_rugby_dups.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# set variables
+calDir="/Users/Daniel/.config/khal/calendars/"
+
+
+# collect data
+cd ${calDir}SuperRugby
+for i in $(ls);
+do
+  awk '/SUMMARY/ {print $0 "," FILENAME}' ${i} >> ${calDir}SRfixtures.dat
+done
+
+# purge duplicates
+rm -v $( awk -F, 'arr[$1]++{print $2}' ${calDir}SRfixtures.dat )
+
+# clean up
+rm -v ${calDir}SRfixtures.dat
+cd - > /dev/null
