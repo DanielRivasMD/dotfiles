e057393b8c8413638127bd031138d802a38a3301
DanielRivasMD <danielrivasmd@gmail.com>
2020-07-17 16:14:37 +0200

    patch pier remotes

--- tmux/sessions/Bianca.sh
+++ tmux/sessions/Bianca.sh
@@ -9,9 +9,9 @@ tmux new-session \
   send-keys 'pier cpu' C-m \; \
   select-pane -t 1 \; \
   new-window -n 'channel' \; \
-  send-keys 'pier sftpBianca' C-m \; \
+  send-keys 'pier sftp-bianca' C-m \; \
   new-window -n 'remote' \; \
-  send-keys 'pier sshBianca' C-m \; \
+  send-keys 'pier ssh-bianca' C-m \; \
   select-window -t 1 \;
 
 # ==============================================
--- tmux/sessions/Uppmax.sh
+++ tmux/sessions/Uppmax.sh
@@ -9,9 +9,9 @@ tmux new-session \
   send-keys 'pier cpu' C-m \; \
   select-pane -t 1 \; \
   new-window -n 'channel' \; \
-  send-keys 'pier sftpUpp' C-m \; \
+  send-keys 'pier sftp-uppmax' C-m \; \
   new-window -n 'remote' \; \
-  send-keys 'pier sshUpp' C-m \; \
+  send-keys 'pier ssh-uppmax' C-m \; \
   select-window -t 1 \;
 
 # ==============================================
