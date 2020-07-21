2e36afb9bbbc4c80b6c35cf7da3d4b055e23aedf
DanielRivasMD <danielrivasmd@gmail.com>
2020-07-21 11:59:22 +0200

    archive profiles & link

--- .archive/bash/bash_prompt.sh
+++
@@ -1,1 +0,0 @@
-local_bash_prompt.sh
--- .archive/zsh/zsh_plugins.sh
+++ .archive/zsh/zsh_plugins.sh
@@ -1,1 +1,8 @@
-local_zsh_plugins.sh
+source /Users/drivas/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+fpath+=( /Users/drivas/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting )
+source /Users/drivas/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /Users/drivas/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /Users/drivas/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /Users/drivas/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source /Users/drivas/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-wfxr-SLASH-forgit/forgit.plugin.zsh
+fpath+=( /Users/drivas/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-wfxr-SLASH-forgit )
