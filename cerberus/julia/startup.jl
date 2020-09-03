1d5bf0a815731e633aa6caa8714f0f9636f7d70c
DanielRivasMD <danielrivasmd@gmail.com>
2020-09-03 10:41:22 +0200

    use Pipe package on startup julia

--- cerberus/julia/startup.jl
+++ cerberus/julia/startup.jl
@@ -1,7 +1,15 @@
 atreplinit() do repl
-    try
-        @eval using OhMyREPL
-    catch e
-        @warn "error while importing OhMyREPL" e
-    end
+
+  try
+    @eval using OhMyREPL
+  catch e
+    @warn "error while importing OhMyREPL" e
+  end
+
+  try
+    @eval using Pipe
+  catch e
+    @warn "error while importing Pipe" e
+  end
+
 end
