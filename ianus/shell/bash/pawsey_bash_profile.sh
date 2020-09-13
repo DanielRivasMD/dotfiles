8f739d61f1278f201675911c412023f1314a620c
DanielRivasMD <danielrivasmd@gmail.com>
2020-09-13 22:57:40 +0200

    pawsey bash

---
+++ ianus/shell/bash/pawsey_bash_profile.sh
@@ -0,0 +1,71 @@
+
+# ==========================
+# ===  Pawsey default    ===
+# ==========================
+
+# ==========================
+# ===  General settings  ===
+# ==========================
+
+# Load General profile
+if [ -f ${HOME}/.profile ]; then
+  . ${HOME}/.profile
+fi
+
+BDOTDIR=${HOME}/.bash
+
+# ==========================
+# ===   aliases          ===
+# ==========================
+
+if [ -f ${IANUS}shell/terminal/pawsey_aliases.sh ]; then
+  . ${IANUS}shell/terminal/pawsey_aliases.sh
+fi
+
+if [ -f ${BDOTDIR}/bash_aliases.sh ]; then
+  . ${BDOTDIR}/bash_aliases.sh
+fi
+
+# ==========================
+# ===  Prompt            ===
+# ==========================
+
+eval "$(starship init bash)"
+
+# ==========================
+# ===  Completion        ===
+# ==========================
+
+# Fuzzy finder (fzf)
+[ -f ${IANUS}bash/fzf.bash ] && source ${IANUS}bash/fzf.bash
+
+# ==========================
+# ===  History           ===
+# ==========================
+
+HISTFILE=${BDOTDIR}/bash_history
+HISTSIZE=1000000
+SAVEHIST=1000000
+
+# ==========================
+# ===  Silence warning   ===
+# ==========================
+
+export BASH_SILENCE_DEPRECATION_WARNING=1
+
+# ==========================
+# ===  zoxide            ===
+# ==========================
+
+eval "$(~/.cargo/bin/zoxide init bash)"
+
+# ==========================
+# ===  McFly             ===
+# ==========================
+
+if [[ -r "/home/drivas/private/software/mcfly/mcfly.bash" ]]
+then
+  source "/home/drivas/private/software/mcfly/mcfly.bash"
+fi
+
+# ==========================
