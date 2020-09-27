d5015213f208c46956f685a1bccd2c7b193de3ac
DanielRivasMD <danielrivasmd@gmail.com>
2020-09-27 21:00:43 +0200

    add vernacular profile bash

---
+++ ianus/shell/bash/vernacular_bash_profile.sh
@@ -0,0 +1,77 @@
+
+# ==========================
+# ===  General settings  ===
+# ==========================
+
+BDOTDIR=${HOME}/.bash
+
+# ==========================
+# ===   aliases          ===
+# ==========================
+
+if [ -f ${IANUS}shell/terminal/vernacular_aliases.sh ]; then
+  . ${IANUS}shell/terminal/vernacular_aliases.sh
+fi
+
+if [ -f ${BDOTDIR}/bash_aliases.sh ]; then
+  . ${BDOTDIR}/bash_aliases.sh
+fi
+
+# ==========================
+# ===  Prompt            ===
+# ==========================
+
+eval "$(starship init bash)"
+
+# ==========================
+# ===  Completion        ===
+# ==========================
+
+# Git-autocompletion
+[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
+
+# Pandoc autocompletion
+eval "$(pandoc --bash-completion)"
+
+# Fuzzy finder (fzf)
+[ -f ${IANUS}shell/bash/fzf.bash ] && source ${IANUS}shell/bash/fzf.bash
+
+# ==========================
+# ===  History           ===
+# ==========================
+
+HISTFILE=${BDOTDIR}/bash_history
+HISTSIZE=1000000
+SAVEHIST=1000000
+
+# ==========================
+# ===  Silence warning   ===
+# ==========================
+
+export BASH_SILENCE_DEPRECATION_WARNING=1
+
+# ==========================
+# ===  zoxide            ===
+# ==========================
+
+eval "$(zoxide init bash)"
+
+# ==========================
+# ===  McFly             ===
+# ==========================
+
+if [[ -r "$(brew --prefix)/opt/mcfly/mcfly.bash" ]]
+then
+  source "$(brew --prefix)/opt/mcfly/mcfly.bash"
+fi
+
+# ==========================
+
+echo -n -e "\t"; echo ''
+echo -n -e "\t"; echo '   ___  ___   ______ __'
+echo -n -e "\t"; echo '  / _ )/ _ | / __/ // /'
+echo -n -e "\t"; echo ' / _  / __ |_\ \/ _  /'
+echo -n -e "\t"; echo '/____/_/ |_/___/_//_/'
+echo -n -e "\t"; echo ''
+
+# ==========================
--- ianus/shell/bash/vernacular_bashrc.sh
+++ ianus/shell/bash/vernacular_bashrc.sh
@@ -3,75 +3,12 @@
 # ===  General settings  ===
 # ==========================
 
-BDOTDIR=${HOME}/.bash
-
-# ==========================
-# ===   aliases          ===
-# ==========================
-
-if [ -f ${IANUS}shell/terminal/vernacular_aliases.sh ]; then
-  . ${IANUS}shell/terminal/vernacular_aliases.sh
-fi
-
-if [ -f ${BDOTDIR}/bash_aliases.sh ]; then
-  . ${BDOTDIR}/bash_aliases.sh
-fi
-
-# ==========================
-# ===  Prompt            ===
-# ==========================
-
-eval "$(starship init bash)"
-
-# ==========================
-# ===  Completion        ===
-# ==========================
-
-# Git-autocompletion
-[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
-
-# Pandoc autocompletion
-eval "$(pandoc --bash-completion)"
-
-# Fuzzy finder (fzf)
-[ -f ${IANUS}shell/bash/fzf.bash ] && source ${IANUS}shell/bash/fzf.bash
-
-# ==========================
-# ===  History           ===
-# ==========================
-
-HISTFILE=${BDOTDIR}/bash_history
-HISTSIZE=1000000
-SAVEHIST=1000000
-
-# ==========================
-# ===  Silence warning   ===
-# ==========================
-
-export BASH_SILENCE_DEPRECATION_WARNING=1
-
-# ==========================
-# ===  zoxide            ===
-# ==========================
-
-eval "$(zoxide init bash)"
-
-# ==========================
-# ===  McFly             ===
-# ==========================
-
-if [[ -r "$(brew --prefix)/opt/mcfly/mcfly.bash" ]]
+if [[ $- == *i* ]]
 then
-  source "$(brew --prefix)/opt/mcfly/mcfly.bash"
+  if [[ -f "$HOME/.archive/ianus/shell/bash/vernacular_bash_profile.sh" ]]
+  then
+    source "$HOME/.archive/ianus/shell/bash/vernacular_bash_profile.sh"
+  fi
 fi
-
-# ==========================
-
-echo -n -e "\t"; echo ''
-echo -n -e "\t"; echo '   ___  ___   ______ __'
-echo -n -e "\t"; echo '  / _ )/ _ | / __/ // /'
-echo -n -e "\t"; echo ' / _  / __ |_\ \/ _  /'
-echo -n -e "\t"; echo '/____/_/ |_/___/_//_/'
-echo -n -e "\t"; echo ''
 
 # ==========================
