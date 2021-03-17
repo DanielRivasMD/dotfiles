d6c91cd4a50894edfdb6008c66787a7bf6e3fcbf
DanielRivasMD <danielrivasmd@gmail.com>
2021-03-17 15:12:11 +0100

    use lg exa git

--- ianus/shell/terminal/vernacular_aliases.sh
+++ ianus/shell/terminal/vernacular_aliases.sh
@@ -53,31 +53,40 @@ alias man=batman
 # broot
 alias br='br --dates --sizes --permissions --hidden --show-gitignored --show-git-info'
 
-# exa
-alias lx='exa --long --classify --links --git --header --group --icons'
-alias lxa='lx --all'
-alias lxr='lx --sort=newest'
-
-alias lxar='lxa --sort=newest'
-alias lxsa='lxa --sort=accessed --time accessed'
-alias lxsc='lxa --sort=created --time created'
-
-alias lxg='lx --group-directories-first'
-alias lxga='lxg --all'
-alias lxgr='lxg --sort=newest'
-
-alias lxgar='lxga --sort=newest'
-alias lxgsa='lxga --sort=accessed --time accessed'
-alias lxgsc='lxga --sort=created --time created'
-
-alias lxt='lxa --tree --level 2 --ignore-glob ".git" --group-directories-first'
-alias lxd='lxa --tree --level 2 --ignore-glob ".git" --only-dirs'
-
 # gitui
 alias gt='gitui'
 
 # git-local-ignore
 alias gl='git-local-ignore'
+
+# samesame
+alias ss='samesame'
+
+# wordcrab
+alias wc='wordcrab'
+
+# zoxide
+alias zz='z -'
+
+# exa
+alias lg='exa --long --classify --links --git --header --group --icons'
+alias lga='lg --all'
+alias lgr='lg --sort=newest'
+
+alias lgar='lga --sort=newest'
+alias lgsa='lga --sort=accessed --time accessed'
+alias lgsc='lga --sort=created --time created'
+
+alias lgg='lg --group-directories-first'
+alias lgga='lgg --all'
+alias lggr='lgg --sort=newest'
+
+alias lggar='lgga --sort=newest'
+alias lggsa='lgga --sort=accessed --time accessed'
+alias lggsc='lgga --sort=created --time created'
+
+alias lgt='lga --tree --level 2 --ignore-glob ".git" --group-directories-first'
+alias lgd='lga --tree --level 2 --ignore-glob ".git" --only-dirs'
 
 # lsd
 alias l='lsd --long --classify'
@@ -99,14 +108,5 @@ alias lta='lt --almost-all'
 
 alias le='l --extensionsort'
 alias lea='le --almost-all'
-
-# samesame
-alias ss='samesame'
-
-# wordcrab
-alias wc='wordcrab'
-
-# zoxide
-alias zz='z -'
 
 # ==========================
