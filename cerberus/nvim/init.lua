48f3ac547a166fb96780d5920bb168c761c89029
DanielRivasMD <danielrivasmd@gmail.com>
2021-03-23 15:41:56 +0100

    nvim. add plugins

--- cerberus/nvim/init.lua
+++ cerberus/nvim/init.lua
@@ -1,12 +1,19 @@
 
+-- lsp
+require('lsp')
+
 require('utils')
 require('plugins')
 require('settings')
 require('keymappings')
 require('colorscheme')
 
--- lsp
-require('lsp')
+-- plugins
+require('plugins.barbar')      -- TODO: customize barbar. tab movement
+require('plugins.galaxyline')  -- TODO: customize galaxyline
+require('plugins.startify')
+require('plugins.compe')
+require('plugins.telescope')   -- TODO: customize
 
 -- vimscript
 vim.cmd('source /Users/drivas/.config/nvim/vimscript/whichkey/init.vim')   -- which key
---
+++ cerberus/nvim/lua/plugins/init.lua
@@ -0,0 +1,59 @@
+
+-------------------- PLUGINS -------------------------------
+local cmd = vim.cmd                     -- to execute Vim commands e.g. cmd('pwd')
+
+cmd 'packadd paq-nvim'                  -- load the package manager
+local paq = require('paq-nvim').paq     -- a convenient alias
+paq {'savq/paq-nvim', opt = true}       -- paq-nvim manages itself
+
+paq {'ayu-theme/ayu-vim'}               -- theme
+paq {'mhinz/vim-startify'}              -- start page
+paq {'glepnir/galaxyline.nvim'}         -- status bar TODO: customize
+paq {'romgrk/barbar.nvim'}              -- tabs
+paq {'kyazdani42/nvim-tree.lua'}        -- tree
+
+paq {'kyazdani42/nvim-web-devicons'}    -- devicons tabs
+paq {'ryanoasis/vim-devicons'}          -- devicons
+
+paq {'rust-lang/rust.vim'}
+paq {'racer-rust/vim-racer'}
+paq {'fatih/vim-go'}
+
+paq {'neovim/nvim-lspconfig'}           -- lsp
+
+paq {'nvim-treesitter/nvim-treesitter'} -- treesitter
+
+paq {'hrsh7th/nvim-compe'}
+
+-- telescope
+paq {'nvim-lua/popup.nvim'}
+paq {'nvim-lua/plenary.nvim'}
+paq {'nvim-telescope/telescope.nvim'}
+
+-- git
+paq {'airblade/vim-gitgutter'}
+paq {'tpope/vim-fugitive'}
+paq {'kshenoy/vim-signature'}
+
+paq {'liuchengxu/vim-which-key'}        -- which key
+
+paq {'Yggdroot/indentline'}
+
+paq {'JuliaEditorSupport/julia-vim'}
+paq {'neovim/nvim-lsp'}
+
+paq {'prabirshrestha/async.vim'}
+paq {'prabirshrestha/asyncomplete.vim'}
+paq {'wellle/tmux-complete.vim'}
+
+paq {'junegunn/fzf', hook = fn['fzf#install']}
+paq {'junegunn/fzf.vim'}
+
+paq {'preservim/nerdcommenter'}
+paq {'godlygeek/tabular'}
+
+--paq {'easymotion/vim-easymotion'}
+
+--     -- -- Easily Create Gists
+--     -- use 'mattn/vim-gist'
+--     -- use 'mattn/webapi-vim'
