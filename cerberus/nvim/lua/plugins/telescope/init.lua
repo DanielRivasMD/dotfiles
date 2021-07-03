f1314d89d178600b6244953c1b7c75d621e82b1d
DanielRivasMD <danielrivasmd@gmail.com>
2021-07-03 20:12:17 +0200

    update telescope config

--- cerberus/nvim/lua/plugins/telescope/init.lua
+++ cerberus/nvim/lua/plugins/telescope/init.lua
@@ -10,7 +10,7 @@ require('telescope').setup {
       '--column',
       '--smart-case'
     },
-    prompt_position = "bottom",
+    -- prompt_position = "bottom",
     prompt_prefix = "  ",
     selection_caret = " ",
     entry_prefix = "  ",
@@ -18,23 +18,15 @@ require('telescope').setup {
     selection_strategy = "reset",
     sorting_strategy = "descending",
     layout_strategy = "horizontal",
-    layout_defaults = {
-      horizontal = {
-        mirror = false
-      },
-      vertical = {
-        mirror = false
-      }
-    },
     file_sorter = require'telescope.sorters'.get_fuzzy_file,
     file_ignore_patterns = {},
     generic_sorter = require'telescope.sorters'.get_generic_fuzzy_sorter,
     shorten_path = true,
     winblend = 0,
-    width = 0.75,
-    preview_cutoff = 120,
-    results_height = 1,
-    results_width = 0.8,
+    -- width = 0.75,
+    -- preview_cutoff = 120,
+    -- results_height = 1,
+    -- results_width = 0.8,
     border = {},
     borderchars = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'},
     color_devicons = true,
@@ -45,5 +37,15 @@ require('telescope').setup {
     qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
 
     buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker,
+
+    layout_config = {
+      horizontal = {
+        mirror = false
+      },
+      vertical = {
+        mirror = false
+      }
+    },
+
   },
 }
