0e57954707526104ea529bac3b98848375c34247
DanielRivasMD <danielrivasmd@gmail.com>
2021-10-14 08:18:54 +0200

    update ls commands & alias

--- ianus/shell/terminal/vernacular_aliases.sh
+++ ianus/shell/terminal/vernacular_aliases.sh
@@ -76,44 +76,44 @@ alias wc='wordcrab'
 alias zz='z -'
 
 # exa
-alias lg='exa --long --classify --links --git --header --group --icons'
+alias l='exa --long --classify --links --git --header --group --icons'
+alias la='l --all'
+alias lr='l --sort=newest'
+
+alias lar='la --sort=newest'
+alias las='la --sort=accessed --time accessed'
+alias lac='la --sort=created --time created'
+
+alias lg='l --group-directories-first'
 alias lga='lg --all'
 alias lgr='lg --sort=newest'
 
 alias lgar='lga --sort=newest'
-alias lgsa='lga --sort=accessed --time accessed'
-alias lgsc='lga --sort=created --time created'
+alias lgas='lga --sort=accessed --time accessed'
+alias lgac='lga --sort=created --time created'
 
-alias lgg='lg --group-directories-first'
-alias lgga='lgg --all'
-alias lggr='lgg --sort=newest'
-
-alias lggar='lgga --sort=newest'
-alias lggsa='lgga --sort=accessed --time accessed'
-alias lggsc='lgga --sort=created --time created'
-
-alias lgt='lga --tree --level 2 --ignore-glob ".git" --group-directories-first'
-alias lgd='lga --tree --level 2 --ignore-glob ".git" --only-dirs'
+alias lt='la --tree --level 2 --ignore-glob ".git" --group-directories-first'
+alias ld='la --tree --level 2 --ignore-glob ".git" --only-dirs'
 
 # lsd
-alias l='lsd --long --classify'
-alias lr='l --reverse --timesort'
-alias lss='l --reverse --sizesort'
-
-alias la='l --almost-all'
-alias lar='la --reverse --timesort'
-alias las='la --reverse --sizesort'
-
-alias ls='l --recursive'
+alias ls='lsd --long --classify'
 alias lsr='ls --reverse --timesort'
+alias lss='ls --reverse --sizesort'
 
 alias lsa='ls --almost-all'
 alias lsar='lsa --reverse --timesort'
+alias lsas='lsa --reverse --sizesort'
 
-alias lt='l --group-dirs=first --tree'
-alias lta='lt --almost-all'
+alias lss='ls --recursive'
+alias lssr='lss --reverse --timesort'
 
-alias le='l --extensionsort'
-alias lea='le --almost-all'
+alias lssa='lss --almost-all'
+alias lssar='lssa --reverse --timesort'
+
+alias lst='ls --group-dirs=first --tree'
+alias lsta='lst --almost-all'
+
+alias lse='ls --extensionsort'
+alias lsea='lse --almost-all'
 
 # ==========================
