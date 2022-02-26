a7e660fbf2dd92025e591905c4c22889132b00c9
DanielRivasMD <danielrivasmd@gmail.com>
2022-02-26 19:24:26 +0100

    update neovim paq

--- cerberus/nvim/lua/plugins/init.lua
+++ cerberus/nvim/lua/plugins/init.lua
@@ -1,65 +1,68 @@
+-------------------- PLUGINS ---------------------
 
--------------------- PLUGINS -------------------------------
-local cmd = vim.cmd                     -- to execute Vim commands e.g. cmd('pwd')
+require 'paq' {
 
-cmd 'packadd paq-nvim'                  -- load the package manager
-local paq = require('paq-nvim').paq     -- a convenient alias
-paq {'savq/paq-nvim', opt = true}       -- paq-nvim manages itself
+  {'savq/paq-nvim', opt = true};                  -- paq-nvim manages itself
 
-paq {'ayu-theme/ayu-vim'}               -- theme
-paq {'mhinz/vim-startify'}              -- start page
-paq {'glepnir/galaxyline.nvim'}         -- status bar
-paq {'romgrk/barbar.nvim'}              -- tabs
-paq {'kyazdani42/nvim-tree.lua'}        -- tree
+  'ayu-theme/ayu-vim';                            -- theme
+  'mhinz/vim-startify';                           -- start page
+  'glepnir/galaxyline.nvim';                      -- status bar
+  'romgrk/barbar.nvim';                           -- tabs
+  'kyazdani42/nvim-tree.lua';                     -- tree
 
-paq {'kyazdani42/nvim-web-devicons'}    -- devicons tabs
-paq {'ryanoasis/vim-devicons'}          -- devicons
+  'kyazdani42/nvim-web-devicons';                 -- devicons tabs
+  'ryanoasis/vim-devicons';                       -- devicons
 
-paq {'rust-lang/rust.vim'}
-paq {'racer-rust/vim-racer'}
---paq {'rust-analyzer/rust-analyzer'}
-paq {'fatih/vim-go'}
+  'neovim/nvim-lspconfig';                        -- lsp
+  'alexaandru/nvim-lspupdate';                    -- lsp manager
+  'nvim-treesitter/nvim-treesitter';              -- treesitter
+  'hrsh7th/nvim-compe';                           -- compe
 
-paq {'neovim/nvim-lspconfig'}           -- lsp
+  'liuchengxu/vim-which-key';                     -- which key
 
-paq {'alexaandru/nvim-lspupdate'}       -- lsp manager
+  -- telescope
+  'nvim-lua/popup.nvim';
+  'nvim-lua/plenary.nvim';
+  'nvim-telescope/telescope.nvim';
 
-paq {'nvim-treesitter/nvim-treesitter'} -- treesitter
+  -- git
+  'airblade/vim-gitgutter';
+  'tpope/vim-fugitive';
+  'kshenoy/vim-signature';
 
-paq {'hrsh7th/nvim-compe'}
+  -- rust
+  'rust-lang/rust.vim';
+  -- 'racer-rust/vim-racer';
+  'rust-analyzer/rust-analyzer';
 
--- telescope
-paq {'nvim-lua/popup.nvim'}
-paq {'nvim-lua/plenary.nvim'}
-paq {'nvim-telescope/telescope.nvim'}
+  -- go
+  'fatih/vim-go';
 
--- git
-paq {'airblade/vim-gitgutter'}
-paq {'tpope/vim-fugitive'}
-paq {'kshenoy/vim-signature'}
+  -- julia
+  'JuliaEditorSupport/julia-vim';
+  'neovim/nvim-lsp';
+  'kdheepak/JuliaFormatter.vim';
 
-paq {'liuchengxu/vim-which-key'}        -- which key
+  'Yggdroot/indentline';
 
-paq {'Yggdroot/indentline'}
+  'prabirshrestha/async.vim';
+  'prabirshrestha/asyncomplete.vim';
+  'wellle/tmux-complete.vim';
 
-paq {'JuliaEditorSupport/julia-vim'}
-paq {'neovim/nvim-lsp'}
-paq {'kdheepak/JuliaFormatter.vim'}
+  {'junegunn/fzf', hook = fn['fzf#install']};
+  'junegunn/fzf.vim';
 
-paq {'prabirshrestha/async.vim'}
-paq {'prabirshrestha/asyncomplete.vim'}
-paq {'wellle/tmux-complete.vim'}
+  'preservim/nerdcommenter';
+  'godlygeek/tabular';
 
-paq {'junegunn/fzf', hook = fn['fzf#install']}
-paq {'junegunn/fzf.vim'}
+  'easymotion/vim-easymotion';
 
-paq {'preservim/nerdcommenter'}
-paq {'godlygeek/tabular'}
+  'jpalardy/vim-slime';
 
-paq {'easymotion/vim-easymotion'}
+  -- -- Easily Create Gists
+  -- use 'mattn/vim-gist';
+  -- use 'mattn/webapi-vim';
 
-paq {'jpalardy/vim-slime'}
+}
 
---     -- -- Easily Create Gists
---     -- use 'mattn/vim-gist'
---     -- use 'mattn/webapi-vim'
+--------------------------------------------------
