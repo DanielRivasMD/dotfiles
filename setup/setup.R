38bc2ce5aff4b018abc67e172437bbde5cda68ca
DanielRivasMD <danielrivasmd@gmail.com>
2022-03-01 12:07:01 +0100

    install R packages for startup

---
+++ setup/setup.R
@@ -0,0 +1,9 @@
+################################################################################
+
+# install remote packages
+install.packages('magrittr')
+install.packages('pacman')
+install.packages('startup')
+install.packages('tidyverse')
+
+################################################################################
