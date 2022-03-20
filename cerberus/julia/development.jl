1fe106399bd7ac715e304ca4cb45afc2fe82e791
DanielRivasMD <danielrivasmd@gmail.com>
2022-03-20 15:51:56 +0100

    add julia development compatible with nvim slime

---
+++ cerberus/julia/development.jl
@@ -0,0 +1,53 @@
+################################################################################
+
+# init call
+atreplinit() do repl
+
+  # only on interactive
+  if isinteractive()
+
+    ################################################################################
+
+    try
+      @eval import Chain: @chain
+    catch e
+      @warn "error while importing Chain" e
+    end
+
+     ################################################################################
+
+    try
+      @eval import Pipe: @pipe
+    catch e
+      @warn "error while importing Pipe" e
+    end
+
+    ################################################################################
+
+    try
+      @eval using Revise
+    catch e
+      @warn "Error initializing Revise" exception = (e, catch_backtrace())
+    end
+
+    ################################################################################
+
+    try
+      @eval using Debugger
+    catch e
+      @warn "Error initializing Debugger" exception = (e, catch_backtrace())
+    end
+
+    ################################################################################
+
+    # julia specific environmental variables
+    ENV["PAGER"] = "less";
+    ENV["VISUAL"] = "subl";
+
+    ################################################################################
+
+  end
+
+end
+
+################################################################################
