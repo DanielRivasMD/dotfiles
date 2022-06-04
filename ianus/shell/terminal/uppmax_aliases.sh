f7e277b06558783fe85076a309c7aea5bbc52cbb
DanielRivasMD <danielrivasmd@gmail.com>
2022-06-04 09:56:45 +0200

    update uppmax aliases

--- ianus/shell/terminal/uppmax_aliases.sh
+++ ianus/shell/terminal/uppmax_aliases.sh
@@ -3,7 +3,6 @@
 ####################################################################################################
 
 # General aliases
-alias c='clear'
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
@@ -16,27 +15,8 @@ alias mv="mv -v"
 alias rm="rm -v"
 alias mkd="mkdir -pv"
 
-####################################################################################################
-# Go toolLinks
-####################################################################################################
-
-
-####################################################################################################
-# Rust toolLinks
-####################################################################################################
-
-# aliases
-alias als='aliases'
-alias alr='clear && aliases rehash && aliases && echo "aliases reloaded"'
-alias ald='clear && aliases directories'
-alias al='aliases exec ./'
-alias a='aliases exec ~/'
-
-# exa
-alias l='clear && exa --long --group-directories-first --classify --links --header --group'
-alias la='clear && exa --long --group-directories-first --classify --links --header --group --all'
-alias lr='clear && exa --long --group-directories-first --classify --links --header --sort=newest --group'
-alias lar='clear && exa --long --group-directories-first --classify --links --header --sort=newest --group --all'
+# hgrep
+alias hg='hgrep'
 
 # samesame
 alias ss='samesame'
@@ -46,6 +26,49 @@ alias wc='wordcrab'
 
 # zoxide
 alias zz='z -'
+
+# exa
+alias e='exa --long --classify --links --git --header --group --icons'
+alias ea='e --all'
+alias er='e --sort=newest'
+
+alias ear='ea --sort=newest'
+alias eas='ea --sort=accessed --time accessed'
+alias eac='ea --sort=created --time created'
+
+alias eg='e --group-directories-first'
+alias ega='eg --all'
+alias egr='eg --sort=newest'
+
+alias egar='ega --sort=newest'
+alias egas='ega --sort=accessed --time accessed'
+alias egac='ega --sort=created --time created'
+
+alias et='ea --tree --level 2 --ignore-glob ".git" --group-directories-first'
+alias ed='ea --tree --level 2 --ignore-glob ".git" --only-dirs'
+
+# lsd
+alias l='lsd --long --classify'
+alias lr='l --reverse --timesort'
+alias lz='l --reverse --sizesort'
+
+alias la='l --almost-all'
+alias lar='la --reverse --timesort'
+alias laz='la --reverse --sizesort'
+
+alias lh='l --recursive'
+alias lhr='lh --reverse --timesort'
+alias lhz='lh --reverse --sizesort'
+
+alias lha='lh --almost-all'
+alias lhar='lha --reverse --timesort'
+alias lhaz='lha --reverse --sizesort'
+
+alias lt='l --group-dirs=first --tree'
+alias lta='lt --almost-all'
+
+alias le='l --extensionsort'
+alias lea='le --almost-all'
 
 ####################################################################################################
 # Uppmax aliases
