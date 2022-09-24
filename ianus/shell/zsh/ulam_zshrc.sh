1dc7b6e4566ecc279e7211375a76eba755728099
DanielRivasMD <danielrivasmd@gmail.com>
2022-09-24 18:11:22 +0200

    add ulam zsh profile

---
+++ ianus/shell/zsh/ulam_zshrc.sh
@@ -0,0 +1,110 @@
+####################################################################################################
+# General settings
+####################################################################################################
+
+ZDOTDIR=${HOME}/.zsh
+
+####################################################################################################
+# aliases
+####################################################################################################
+
+if [ -f ${IANUS}/shell/terminal/ulam_aliases.sh ]; then
+  . ${IANUS}/shell/terminal/ulam_aliases.sh
+fi
+
+if [ -f ${ZDOTDIR}/zsh_aliases.sh ]; then
+  . ${ZDOTDIR}/zsh_aliases.sh
+fi
+
+####################################################################################################
+# Prompt
+####################################################################################################
+
+eval "$(starship init zsh)"
+
+####################################################################################################
+# Plugins AB
+####################################################################################################
+
+# Plugins managed by antibody (AB) statically
+# antibody bundle < ${ZDOTDIR}/zsh_plugins.txt > ${ZDOTDIR}/zsh_plugins.sh
+source ${ZDOTDIR}/zsh_plugins.sh
+
+
+# # Plugins managed by antibody (AB) dinamically
+# # source <(antibody init)
+# zdharma/fast-syntax-highlighting
+# zsh-users/zsh-autosuggestions
+# zsh-users/zsh-completions
+# wfxr/forgit
+# chrissicool/zsh-256color
+
+
+####################################################################################################
+# Autocompletion
+####################################################################################################
+
+# add custom zsh completion path
+fpath=($HOME/.config/zsh_completion $fpath)
+
+# Autocompletion with arrow interphase
+autoload -Uz compinit
+# Dump zcompdump
+compinit -d ~/.cache/zsh/zcompdump-$ZSH_VERSION
+_comp_options+=(globdots)   # Include hidden files.
+zstyle ':completion:*' menu select
+
+setopt COMPLETE_ALIASES
+zstyle ':completion::complete:*' gain-privileges 1
+
+# Case-insensitive
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+
+# Pandoc autocompletion
+source ${ZDOTDIR}/zsh_pandoc_autocompletion.sh
+
+# Fuzzy finder (fzf)
+[ -f ${ZDOTDIR}/fzf.zsh ] && source ${ZDOTDIR}/fzf.zsh
+
+####################################################################################################
+# History
+####################################################################################################
+
+HISTFILE=${ZDOTDIR}/zsh_history
+HISTSIZE=1000000
+SAVEHIST=1000000
+setopt appendhistory
+
+####################################################################################################
+# Key bindings
+####################################################################################################
+
+bindkey "\x1b[1;3D" backward-word
+bindkey "\x1b[1;3C" forward-word
+bindkey "\x01" beginning-of-line
+bindkey "\x05" end-of-line
+
+####################################################################################################
+# zoxide
+####################################################################################################
+
+eval "$(zoxide init zsh)"
+
+####################################################################################################
+# McFly
+####################################################################################################
+
+if [[ -r "/home/drivas/private/software/mcfly/mcfly.zsh" ]]
+then
+  source "/home/drivas/private/software/mcfly/mcfly.zsh"
+fi
+
+####################################################################################################
+# editor
+####################################################################################################
+
+# edit line in editor with ctrl-n:
+autoload edit-command-line; zle -N edit-command-line
+bindkey '^b' edit-command-line
+
+####################################################################################################
