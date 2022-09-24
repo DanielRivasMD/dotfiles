303eedd425bee6d6f9bf4eced7a85be1c48c279b
DanielRivasMD <danielrivasmd@gmail.com>
2022-09-24 18:11:29 +0200

    add ulam bash profile

--- ianus/shell/bash/ulam_bash_profile.sh
+++ ianus/shell/bash/ulam_bash_profile.sh
@@ -1,20 +1,6 @@
 ####################################################################################################
-# Uppmax default
+# Pawsey default
 ####################################################################################################
-
-# Modules, activate the module command
-case "$0" in
-  -sh|sh|*/sh)	modules_shell=sh ;;
-  -ksh|ksh|*/ksh)	modules_shell=ksh ;;
-  -zsh|zsh|*/zsh)	modules_shell=zsh ;;
-  -bash|bash|*/bash)	modules_shell=bash ;;
-esac
-module() { eval `/usr/local/Modules/$MODULE_VERSION/bin/modulecmd $modules_shell $*`; }
-
-# Source global definitions
-if [ -f /etc/bashrc ]; then
-  . /etc/bashrc
-fi
 
 ####################################################################################################
 # General settings
@@ -31,8 +17,8 @@ BDOTDIR=${HOME}/.bash
 # aliases
 ####################################################################################################
 
-if [ -f ${IANUS}/shell/terminal/uppmax_aliases.sh ]; then
-  . ${IANUS}/shell/terminal/uppmax_aliases.sh
+if [ -f ${IANUS}/shell/terminal/ulam_aliases.sh ]; then
+  . ${IANUS}/shell/terminal/ulam_aliases.sh
 fi
 
 if [ -f ${BDOTDIR}/bash_aliases.sh ]; then
