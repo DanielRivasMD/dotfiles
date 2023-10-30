1b98d033f8dc26d8b5149ecec2bbac8ddce971b4
DanielRivasMD <danielrivasmd@gmail.com>
2023-10-30 16:51:07 -0600

    add zoxide binding

---
+++ ianus/shell/zsh/zsh_zoxide.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env zsh
+
+####################################################################################################
+
+_call_zi() {
+  local dir="$(eval "zoxide query -i")"
+
+  if [[ -z "$dir" ]]; then
+      zle redisplay
+      return 0
+  fi
+
+  zle push-line
+  BUFFER="cd -- ${(q)dir}"
+  zle accept-line
+  local ret=$?
+  unset dir
+  zle reset-prompt
+  return $ret
+}
+
+zle -N _call_zi
+
+####################################################################################################
