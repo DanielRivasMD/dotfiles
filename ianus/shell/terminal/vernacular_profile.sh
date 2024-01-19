e1ab1d27318ea2f317e1f51744147df2e92028df
DanielRivasMD <danielrivasmd@gmail.com>
2024-01-19 17:02:39 -0600

    update goroot vernacular profile

--- ianus/shell/terminal/vernacular_profile.sh
+++ ianus/shell/terminal/vernacular_profile.sh
@@ -39,7 +39,7 @@ export LC_ALL=en_US.UTF-8
 ####################################################################################################
 
 export GOPATH="$HOME/.go/"
-export GOROOT="/usr/local/Cellar/go/1.21.5/libexec"
+export GOROOT="/opt/homebrew/Cellar/go/1.21.6/libexec"
 export PATH="$GOPATH/bin:$PATH"; # g-install: do NOT edit, see https://github.com/stefanmaric/g
 
 ####################################################################################################
