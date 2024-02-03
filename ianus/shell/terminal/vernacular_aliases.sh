e6d98ccdeb06d868a126983632213f61159fe32b
DanielRivasMD <danielrivasmd@gmail.com>
2024-02-03 17:45:07 -0600

    update aliases

--- ianus/shell/terminal/vernacular_aliases.sh
+++ ianus/shell/terminal/vernacular_aliases.sh
@@ -9,8 +9,8 @@ alias rip='~/bin/forkedPatch/rip'
 # python
 ####################################################################################################
 
-alias pip=pip3
-alias python=python3
+alias pip='pip3'
+alias python='python3'
 
 ####################################################################################################
 # general aliases
@@ -20,15 +20,15 @@ alias python=python3
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
-alias c=clear
+alias c='clear'
 
 ####################################################################################################
 
 # increase verbosity
-alias cp="cp -v"
-alias mv="mv -v"
-alias rm="rm -v"
-alias mkd="mkdir -pv"
+alias cp='cp -v'
+alias mv='mv -v'
+alias rm='rm -v'
+alias mk='mkdir -pv'
 
 ####################################################################################################
 
@@ -42,6 +42,7 @@ alias mp='mplayer'
 alias f='fillin'
 alias lzg='lazygit'
 alias lzd='lazydocker'
+alias lzn='lazynpm'
 
 ####################################################################################################
 # rust tool links
@@ -58,7 +59,7 @@ alias br='br --dates --sizes --permissions --hidden --git-ignored --show-git-inf
 alias bs='br --git-status'
 
 # gfold
-alias gf='~/.cargo/bin/gfold'
+alias gf='gfold'
 
 # gitui
 alias gt='gitui'
@@ -79,46 +80,17 @@ alias wc='wordcrab'
 alias zz='z -'
 
 # eza
-alias e='eza --long --classify --links --git --header --group --icons'
+alias e='eza --header --long --icons --links --classify --git --total-size --group'
 alias ea='e --all'
 alias er='e --sort=newest'
-
 alias ear='ea --sort=newest'
-alias eas='ea --sort=accessed --time accessed'
-alias eac='ea --sort=created --time created'
-
-alias eg='e --group-directories-first'
-alias ega='eg --all'
-alias egr='eg --sort=newest'
-
-alias egar='ega --sort=newest'
-alias egas='ega --sort=accessed --time accessed'
-alias egac='ega --sort=created --time created'
-
 alias et='ea --tree --level 2 --ignore-glob ".git" --group-directories-first'
-alias ed='ea --tree --level 2 --ignore-glob ".git" --only-dirs'
 
 # lsd
-alias l='ls --long --classify'
-alias lr='l --reverse --timesort'
-alias lz='l --reverse --sizesort'
-
+alias l='lsd --header --long --classify --git --total-size'
 alias la='l --almost-all'
-alias lar='la --reverse --timesort'
-alias laz='la --reverse --sizesort'
-
-alias lh='l --recursive'
-alias lhr='lh --reverse --timesort'
-alias lhz='lh --reverse --sizesort'
-
-alias lha='lh --almost-all'
-alias lhar='lha --reverse --timesort'
-alias lhaz='lha --reverse --sizesort'
-
-alias lt='l --group-dirs=first --tree'
-alias lta='lt --almost-all'
-
-alias le='l --extensionsort'
-alias lea='le --almost-all'
+alias lr='l --timesort --reverse'
+alias lar='la --timesort --reverse'
+alias lt='la --tree --ignore-glob ".git" --group-directories-first'
 
 ####################################################################################################
