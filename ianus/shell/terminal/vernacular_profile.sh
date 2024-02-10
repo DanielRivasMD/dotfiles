cb9cdc8c39dc6171acadceb994fdd9933bc89477
DanielRivasMD <danielrivasmd@gmail.com>
2024-02-10 11:25:31 -0600

    update golang version shell

--- ianus/shell/terminal/vernacular_profile.sh
+++ ianus/shell/terminal/vernacular_profile.sh
@@ -40,7 +40,7 @@ export LC_ALL=en_US.UTF-8
 
 export GOPATH="$HOME/.go/"
 export PATH="$GOPATH/bin:$PATH"; # g-install: do NOT edit, see https://github.com/stefanmaric/g
-export GOROOT="$(brew --prefix)/Cellar/go/1.21.6/libexec"
+export GOROOT="$(brew --prefix)/Cellar/go/1.21.7/libexec"
 
 ####################################################################################################
 #  cargo-rust path
