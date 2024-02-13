db517b88db9d9095383986f8c6f7f2b0b309b528
DanielRivasMD <danielrivasmd@gmail.com>
2024-02-13 12:22:00 -0600

    add helix just

---
+++ .just/helix.sh
@@ -0,0 +1,40 @@
+####################################################################################################
+# Helix
+####################################################################################################
+
+#!/bin/sh
+
+####################################################################################################
+
+# declarations
+source .just/.config.sh
+
+####################################################################################################
+
+# create temporary files
+awk -v mode="normal_mode" '{gsub("XXX_MODE", mode); print $0}' "${moded}/common.toml" > "${helix}/.normal.tmp"
+awk -v mode="insert_mode" '{gsub("XXX_MODE", mode); print $0}' "${moded}/common.toml" > "${helix}/.insert.tmp"
+awk -v mode="select_mode" '{gsub("XXX_MODE", mode); print $0}' "${moded}/common.toml" > "${helix}/.select.tmp"
+
+####################################################################################################
+
+# concatenate
+cat \
+  "${moded}/theme.toml" \
+  "${moded}/editor.toml" \
+  "${moded}/normal.toml" \
+  "${helix}/.normal.tmp" \
+  "${moded}/insert.toml" \
+  "${helix}/.insert.tmp" \
+  "${moded}/select.toml" \
+  "${helix}/.select.tmp" \
+  > "${helix}/config.toml"
+
+####################################################################################################
+
+# purge temporary files
+rm "${helix}/.normal.tmp"
+rm "${helix}/.insert.tmp"
+rm "${helix}/.select.tmp"
+
+####################################################################################################
