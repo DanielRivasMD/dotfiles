ced09fefa202abb9a92accec9b5487a0f6280f6b
DanielRivasMD <danielrivasmd@gmail.com>
2024-03-03 09:57:16 -0600

    colorize ianus just

--- .just/ianus.sh
+++ .just/ianus.sh
@@ -18,105 +18,109 @@
 # The ancient Greeks had no equivalent to Janus, whom the Romans claimed as distinctively their own.
 ####################################################################################################
 
-# declarations
+# config
 source .just/.config.sh
 
+####################################################################################################
+# link
 ####################################################################################################
 
 # R
 echo ''
-echo 'Linking R config'
-echo '===================================================================================================='
-if [[ -d "${home}/.Rprofile.d" ]]; then rm -rf "${home}/.Rprofile.d"; fi      # purge before linking
-ln -svf "${ianus}/R/Rprofile.d" "${home}/.Rprofile.d"                         # rprofile directory
-ln -svf "${ianus}/R/vernacular_Rprofile.R" "${home}/.Rprofile"                # rprofile
-echo '===================================================================================================='
+echo "${YELLOW}Linking R config${NC}"
+echo "${RED}====================================================================================================${NC}"
+if [[ -d "${home}/.Rprofile.d" ]]; then rm -rf "${home}/.Rprofile.d"; fi        && echo "Directory created ${BROWN}=>${NC} ${GREEN}Rprofile${NC}"
+ln -sf "${ianus}/R/Rprofile.d" "${home}/.Rprofile.d"                            && echo "Linked ${BROWN}=>${NC} ${BLUE}Rprofile${NC} directory"
+ln -sf "${ianus}/R/vernacular_Rprofile.R" "${home}/.Rprofile"                   && echo "Linked ${BROWN}=>${NC} ${BLUE}Rprofile${NC}"
+echo "${RED}====================================================================================================${NC}"
 
 # @HOME
 echo ''
-echo 'Linking config @HOME'
-echo '===================================================================================================='
-ln -svf "${pier}/pier.toml" "${home}/.pier.toml"                              # pier
-ln -svf "${screen}/4.08.00.screenrc" "${home}/.screenrc"                      # screen
-echo '===================================================================================================='
+echo "${YELLOW}Linking config @HOME${NC}"
+echo "${RED}====================================================================================================${NC}"
+ln -sf "${pier}/pier.toml" "${home}/.pier.toml"                                 && echo "Linked ${BROWN}=>${NC} ${BLUE}pier${NC}"
+ln -sf "${screen}/4.08.00.screenrc" "${home}/.screenrc"                         && echo "Linked ${BROWN}=>${NC} ${BLUE}screen${NC}"
+echo "${RED}====================================================================================================${NC}"
 
 # @config
 echo ''
-echo 'Linking config @config'
-echo '===================================================================================================='
-ln -svf "${micro}" "${config}"                                                # micro directory
-ln -svf "${helix}" "${config}"                                                # helix config
-ln -svf "${lsd}" "${config}"                                                  # lsd directory
-ln -svf "${sheldon}" "${config}"                                              # sheldon directory
-ln -svf "${starship}" "${config}"                                             # starship directory
-echo '===================================================================================================='
+echo "${YELLOW}Linking config @config${NC}"
+echo "${RED}====================================================================================================${NC}"
+ln -sf "${micro}" "${config}"                                                   && echo "Linked ${BROWN}=>${NC} ${BLUE}micro${NC} directory"
+ln -sf "${helix}" "${config}"                                                   && echo "Linked ${BROWN}=>${NC} ${BLUE}helix${NC} config"
+ln -sf "${lsd}" "${config}"                                                     && echo "Linked ${BROWN}=>${NC} ${BLUE}lsd${NC} directory"
+ln -sf "${sheldon}" "${config}"                                                 && echo "Linked ${BROWN}=>${NC} ${BLUE}sheldon${NC} directory"
+ln -sf "${starship}" "${config}"                                                && echo "Linked ${BROWN}=>${NC} ${BLUE}starship${NC} directory"
+echo "${RED}====================================================================================================${NC}"
 
 # shell
 echo ''
-echo 'Linking terminal profile'
-echo '===================================================================================================='
-ln -svf "${terminal}/vernacular_profile.sh" "${home}/.profile"                # terminal profile
-echo '===================================================================================================='
+echo "${YELLOW}Linking terminal profile${NC}"
+echo "${RED}====================================================================================================${NC}"
+ln -sf "${terminal}/vernacular_profile.sh" "${home}/.profile"                   && echo "Linked ${BROWN}=>${NC} ${BLUE}terminal${NC} profile"
+echo "${RED}====================================================================================================${NC}"
 
 # bash
 echo ''
-echo 'Linking bash profile'
-echo '===================================================================================================='
-if [[ ! -d "${home}/.bash" ]]; then mkdir "${home}/.bash"; fi                 # purge before linking
-ln -svf "${bash}/vernacular_bashrc.sh" "${home}/.bashrc"                      # bashrc
-ln -svf "${bash}/vernacular_bash_aliases.sh" "${home}/.bash/bash_aliases.sh"  # bash aliases
-ln -svf "${bash}/fzf.bash" "${home}/.bash"                                    # fzf bash
-ln -svf "${bash}/bash_navi_patch.sh" "${home}/.bash"                          # bash navi
-echo '===================================================================================================='
+echo "${YELLOW}Linking bash profile${NC}"
+echo "${RED}====================================================================================================${NC}"
+if [[ ! -d "${home}/.bash" ]]; then mkdir "${home}/.bash"; fi                   && echo "Directory created ${BROWN}=>${NC} ${GREEN}bash${NC}"
+ln -sf "${bash}/vernacular_bashrc.sh" "${home}/.bashrc"                         && echo "Linked ${BROWN}=>${NC} ${BLUE}bashrc${NC}"
+ln -sf "${bash}/vernacular_bash_aliases.sh" "${home}/.bash/bash_aliases.sh"     && echo "Linked ${BROWN}=>${NC} ${BLUE}bash aliases${NC}"
+ln -sf "${bash}/fzf.bash" "${home}/.bash"                                       && echo "Linked ${BROWN}=>${NC} ${BLUE}fzf bash${NC}"
+ln -sf "${bash}/bash_navi_patch.sh" "${home}/.bash"                             && echo "Linked ${BROWN}=>${NC} ${BLUE}bash navi${NC}"
+echo "${RED}====================================================================================================${NC}"
 
 # zsh
 echo ''
-echo 'Linking zsh profile'
-echo '===================================================================================================='
-if [[ ! -d "${home}/.zsh" ]]; then mkdir "${home}/.zsh"; fi                   # purge before linking
-ln -svf "${zsh}/vernacular_zshrc.sh" "${home}/.zshrc"                         # zshrc
-ln -svf "${zsh}/vernacular_zsh_aliases.sh" "${home}/.zsh/zsh_aliases.sh"      # zsh aliases
-ln -svf "${zsh}/fzf.zsh" "${home}/.zsh"                                       # fzf zsh
-ln -svf "${zsh}/zsh_just.sh" "${home}/.zsh"                                   # zsh just
-ln -svf "${zsh}/zsh_navi.sh" "${home}/.zsh"                                   # zsh navi
-ln -svf "${zsh}/zsh_zellij.sh" "${home}/.zsh"                                 # zsh zellij
-ln -svf "${zsh}/zsh_zoxide.sh" "${home}/.zsh"                                 # zsh zoxide
-echo '===================================================================================================='
+echo "${YELLOW}Linking zsh profile${NC}"
+echo "${RED}====================================================================================================${NC}"
+if [[ ! -d "${home}/.zsh" ]]; then mkdir "${home}/.zsh"; fi                     && echo "Directory created ${BROWN}=>${NC} ${GREEN}zsh${NC}"
+ln -sf "${zsh}/vernacular_zshrc.sh" "${home}/.zshrc"                            && echo "Linked ${BROWN}=>${NC} ${BLUE}zshrc${NC}"
+ln -sf "${zsh}/vernacular_zsh_aliases.sh" "${home}/.zsh/zsh_aliases.sh"         && echo "Linked ${BROWN}=>${NC} ${BLUE}zsh aliases${NC}"
+ln -sf "${zsh}/fzf.zsh" "${home}/.zsh"                                          && echo "Linked ${BROWN}=>${NC} ${BLUE}fzf zsh${NC}"
+ln -sf "${zsh}/zsh_just.sh" "${home}/.zsh"                                      && echo "Linked ${BROWN}=>${NC} ${BLUE}zsh just${NC}"
+ln -sf "${zsh}/zsh_navi.sh" "${home}/.zsh"                                      && echo "Linked ${BROWN}=>${NC} ${BLUE}zsh navi${NC}"
+ln -sf "${zsh}/zsh_zellij.sh" "${home}/.zsh"                                    && echo "Linked ${BROWN}=>${NC} ${BLUE}zsh zellij${NC}"
+ln -sf "${zsh}/zsh_zoxide.sh" "${home}/.zsh"                                    && echo "Linked ${BROWN}=>${NC} ${BLUE}zsh zoxide${NC}"
+echo "${RED}====================================================================================================${NC}"
 
 # nushell
 echo ''
-echo 'Linking nushell profile'
-echo '===================================================================================================='
-ln -svf "${nushell}/env.nu" "${nushellConf}/"                                 # nushell env
-ln -svf "${nushell}/config.nu" "${nushellConf}/"                              # nushell config
-echo '===================================================================================================='
+echo "${YELLOW}Linking nushell profile${NC}"
+echo "${RED}====================================================================================================${NC}"
+ln -sf "${nushell}/env.nu" "${nushellConf}/"                                    && echo "Linked ${BROWN}=>${NC} ${BLUE}nushell env${NC}"
+ln -sf "${nushell}/config.nu" "${nushellConf}/"                                 && echo "Linked ${BROWN}=>${NC} ${BLUE}nushell config${NC}"
+echo "${RED}====================================================================================================${NC}"
 
 ####################################################################################################
-# Completions
+# completions
+####################################################################################################
 
 echo ''
-echo 'Creating completion directory'
-echo '===================================================================================================='
-if [[ ! -d "${zshcomp}" ]]; then mkdir "${zshcomp}"; fi && echo 'Directory ready'
-echo '===================================================================================================='
+echo "${YELLOW}Creating completion directory${NC}"
+echo "${RED}====================================================================================================${NC}"
+if [[ ! -d "${zshcomp}" ]]; then mkdir "${zshcomp}"; fi                         && echo "Directory created ${BROWN}=>${NC} ${GREEN}zsh_completion${NC}"
+echo "${RED}====================================================================================================${NC}"
 
 echo ''
-echo 'Generate completions'
-echo '===================================================================================================='
-pueue completions zsh "${zshcomp}" && echo 'Setup pueue'
-cp -v "${zshcomp}/_pueue" "${zshcomp}/_p" && sd pueue p "${zshcomp}/_p"
-sheldon completions --shell zsh > "${zshcomp}/_sheldon" && echo 'Setup sheldon'
-echo '===================================================================================================='
+echo "${YELLOW}Generate completions${NC}"
+echo "${RED}====================================================================================================${NC}"
+pueue completions zsh "${zshcomp}"                                              && echo "Installed completions ${BROWN}=>${NC} ${CYAN}pueue${NC}"
+cp "${zshcomp}/_pueue" "${zshcomp}/_p" && sd pueue p "${zshcomp}/_p"            && echo "Installed completions ${BROWN}=>${NC} ${CYAN}p${NC}"
+sheldon completions --shell zsh > "${zshcomp}/_sheldon"                         && echo "Installed completions ${BROWN}=>${NC} ${CYAN}sheldon${NC}"
+echo "${RED}====================================================================================================${NC}"
 
-echo '===================================================================================================='
-bender completion zsh > "${zshcomp}/_bender" && echo 'Setup bender'
-hercules completion zsh > "${zshcomp}/_hercules" && echo 'Setup hercules'
-janus completion zsh > "${zshcomp}/_janus" && echo 'Setup janus'
-kali completion zsh > "${zshcomp}/_kali" && echo 'Setup kali'
-lou completion zsh > "${zshcomp}/_lou" && echo 'Setup lou'
-mbombo completion zsh > "${zshcomp}/_mbombo" && echo 'Setup mbombo'
-minerva completion zsh > "${zshcomp}/_minerva" && echo 'Setup minerva'
-tabularasa completion zsh > "${zshcomp}/_tabularasa" && echo 'Setup tabularasa'
-echo '===================================================================================================='
+echo ''
+echo "${RED}====================================================================================================${NC}"
+bender completion zsh > "${zshcomp}/_bender"                                    && echo "Installed completions ${BROWN}=>${NC} ${CYAN}bender${NC}"
+hercules completion zsh > "${zshcomp}/_hercules"                                && echo "Installed completions ${BROWN}=>${NC} ${CYAN}hercules${NC}"
+janus completion zsh > "${zshcomp}/_janus"                                      && echo "Installed completions ${BROWN}=>${NC} ${CYAN}janus${NC}"
+kali completion zsh > "${zshcomp}/_kali"                                        && echo "Installed completions ${BROWN}=>${NC} ${CYAN}kali${NC}"
+lou completion zsh > "${zshcomp}/_lou"                                          && echo "Installed completions ${BROWN}=>${NC} ${CYAN}lou${NC}"
+mbombo completion zsh > "${zshcomp}/_mbombo"                                    && echo "Installed completions ${BROWN}=>${NC} ${CYAN}mbombo${NC}"
+minerva completion zsh > "${zshcomp}/_minerva"                                  && echo "Installed completions ${BROWN}=>${NC} ${CYAN}minerva${NC}"
+tabularasa completion zsh > "${zshcomp}/_tabularasa"                            && echo "Installed completions ${BROWN}=>${NC} ${CYAN}tabularasa${NC}"
+echo "${RED}====================================================================================================${NC}"
 
 ####################################################################################################
