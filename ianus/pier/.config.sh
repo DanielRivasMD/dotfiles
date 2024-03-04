d9e8c4d430342a66b98f5434b3ccf07e91acc5fe
DanielRivasMD <danielrivasmd@gmail.com>
2024-03-04 14:51:20 -0600

    add config pier

---
+++ ianus/pier/.config.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+####################################################################################################
+# config
+####################################################################################################
+
+# home
+export home="${HOME}"
+export config="${home}/.config"
+export archive="${home}/.archive"
+export cerberus="${archive}/cerberus"
+export ianus="${archive}/ianus"
+export forge="${archive}/.just/.forge"
+export update="${archive}/.just/.update"
+export forked="${home}/Forked"
+export observatory="${home}/Observatory"
+export appSupport="${home}/Library/Application Support"
+export localShare="${home}/.local/share"
+export completionsDir="${archive}/completions"
+
+####################################################################################################
+
+# pueue
+export pueuedTracker="${appSupport}/pueue/pueue.pid"
+export pueueWGroup="forgeMaster"
+export pueueUGroup="update"
+
+####################################################################################################
+
+# colors
+export NC='\033[0m'
+export BLACK='\033[0;30m'
+export GRAY='\033[1;30m'
+export RED='\033[0;31m'
+export LIGHT_RED='\033[1;31m'
+export GREEN='\033[0;32m'
+export LIGHT_GREEN='\033[1;32m'
+export BROWN='\033[0;33m'
+export YELLOW='\033[1;33m'
+export BLUE='\033[0;34m'
+export LIGHT_BLUE='\033[1;34m'
+export PURPLE='\033[0;35m'
+export LIGHT_PURPLE='\033[1;35m'
+export CYAN='\033[0;36m'
+export LIGHT_CYAN='\033[1;36m'
+export LIGHT_GRAY='\033[0;37m'
+export WHITE='\033[1;37m'
+
+
+####################################################################################################
