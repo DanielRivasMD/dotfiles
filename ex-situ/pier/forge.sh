3e1fbe1f64a900350155c567033b38716774e87f
DanielRivasMD <danielrivasmd@gmail.com>
2024-03-05 10:23:37 -0600

    update forge pier

--- ex-situ/pier/forge.sh
+++ ex-situ/pier/forge.sh
@@ -2,34 +2,44 @@
 ####################################################################################################
 
 # config
-source $HOME/.archive/ex-situ/pier/.config.sh
+source "${ARCHIVE}/.config/config.sh"
 
 ####################################################################################################
-# deamon
+# daemon
 ####################################################################################################
 
 # control daemon
-source "${forge}/.daemon.sh"
+source "${PIER}/.daemon.sh"
+if check_daemon
+then
+  # create group
+  pueue group add "${pueueFG}"
+
+  # parallel jobs
+  pueue parallel --group "${pueueFG}" 3
+else
+  exit
+fi
 
 ####################################################################################################
 # alacritty
 ####################################################################################################
 
 # watch alacritty
-pueue add --group "${pueueWGroup}" -- watchexec --watch ~/.archive/in-situ/alacritty/alacritty.yml -- 'source ~/.archive/ex-situ/pier/.forge/alacritty.sh'
+pueue add --group "${pueueFG}" -- watchexec --watch ~/.archive/in-situ/alacritty/alacritty.yml -- 'source ~/.archive/ex-situ/pier/.forge/alacritty.sh'
 
 ####################################################################################################
 # goku
 ####################################################################################################
 
 # format & watch goku
-pueue add --group "${pueueWGroup}" -- watchexec --watch ~/.archive/in-situ/karabiner/fragmented -- 'source ~/.archive/ex-situ/pier/.forge/goku.sh'
+pueue add --group "${pueueFG}" -- watchexec --watch ~/.archive/in-situ/karabiner/fragmented -- 'source ~/.archive/ex-situ/pier/.forge/goku.sh'
 
 ####################################################################################################
 # helix
 ####################################################################################################
 
 # format & watch helix
-pueue add --group "${pueueWGroup}" -- watchexec --watch ~/.archive/ex-situ/helix/modes -- 'source ~/.archive/ex-situ/pier/.forge/helix.sh'
+pueue add --group "${pueueFG}" -- watchexec --watch ~/.archive/ex-situ/helix/modes -- 'source ~/.archive/ex-situ/pier/.forge/helix.sh'
 
 ####################################################################################################
