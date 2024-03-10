cfefcfa49469eb82eb99454717d142f0d0dfedcd
DanielRivasMD <danielrivasmd@gmail.com>
2024-03-09 20:12:39 -0600

    update helix forge

--- ex-situ/pier/.forge/helix.sh
+++ ex-situ/pier/.forge/helix.sh
@@ -9,9 +9,9 @@ function _forge() {
   source "${IN_SILICO}/config.sh"
 
   # create temporary files
-  awk -v mode="normal_mode" '{gsub("XXX_MODE", mode); print $0}' "${moded}/common.toml" > "${helix}/.normal.tmp"
-  awk -v mode="insert_mode" '{gsub("XXX_MODE", mode); print $0}' "${moded}/common.toml" > "${helix}/.insert.tmp"
-  awk -v mode="select_mode" '{gsub("XXX_MODE", mode); print $0}' "${moded}/common.toml" > "${helix}/.select.tmp"
+  awk -v mode="normal_mode" '{gsub("MODE", mode); print $0}' "${moded}/common.toml" > "${helix}/.normal.tmp"
+  awk -v mode="insert_mode" '{gsub("MODE", mode); print $0}' "${moded}/common.toml" > "${helix}/.insert.tmp"
+  awk -v mode="select_mode" '{gsub("MODE", mode); print $0}' "${moded}/common.toml" > "${helix}/.select.tmp"
 
   # concatenate
   cat \
