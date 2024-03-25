1e491c2fe2b62adaabb13235ba53890dc6ff2c65
DanielRivasMD <danielrivasmd@gmail.com>
2024-03-24 21:39:12 -0600

    update aliases zsh shell

--- ex-situ/shell/terminal/vernacular_aliases.sh
+++ ex-situ/shell/terminal/vernacular_aliases.sh
@@ -55,14 +55,14 @@ alias activate='source venv/bin/activate'
 
 # calendar
 alias cal='khal calendar'
-alias cal-astra='khal calendar --include-calendar AstraZeneca'
+alias cal-az='khal calendar --include-calendar AZ'
 alias cal-astro='khal calendar --include-calendar Astronomy'
 alias cal-funding='khal calendar --include-calendar Funding'
 alias cal-igneous='khal calendar --include-calendar Igneous'
 alias cal-kukulkan='khal calendar --include-calendar Kukulkan'
 alias cal-personal='khal calendar --include-calendar Personal'
 alias cal-sports='khal calendar --include-calendar Sports'
-alias cal-work='khal calendar --exclude-calendar Astronomy --exclude-calendar Sports'
+alias cal-work='khal calendar --exclude-calendar Astronomy --exclude-calendar Sports --exclude-calendar Personal'
 alias ical='ikhal'
 
 # package manager & repl
