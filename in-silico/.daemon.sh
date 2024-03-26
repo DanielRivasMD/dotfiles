82ea1349273802a2f727bc3c070890eac5832258
DanielRivasMD <danielrivasmd@gmail.com>
2024-03-26 07:03:35 -0600

    add daemon

---
+++ in-silico/.daemon.sh
@@ -0,0 +1,20 @@
+####################################################################################################
+
+# check daemon status
+check_daemon() {
+  if test ! -f "${pueuedTracker}" && test ! -f "${pueueSocket}"
+  then
+    source "${IN_SILICO}/breakCross.sh" $COLUMNS
+    echo ''
+    echo "${CYAN}pueue${YELLOW} daemon${NC} is not currently running${NC}"
+    echo ''
+    echo "start by calling ${CYAN}pueued${GREEN} --daemonize${NC}"
+    echo ''
+    source "${IN_SILICO}/breakCross.sh" $COLUMNS
+    return 1
+  else
+    return 0
+  fi
+}
+
+####################################################################################################
