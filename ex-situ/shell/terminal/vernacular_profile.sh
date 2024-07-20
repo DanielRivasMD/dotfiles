5ba2baf4fd03b77ac32c11dbbe4b7d75804948ea
DanielRivasMD <danielrivasmd@gmail.com>
2024-07-20 14:53:59 -0600

    add automatic go root & path vernacular profile

--- ex-situ/shell/terminal/vernacular_profile.sh
+++ ex-situ/shell/terminal/vernacular_profile.sh
@@ -13,7 +13,7 @@ stty -ixon
 PATH=$PATH:$HOME/.cargo/bin
 
 # go
-export PATH="$GOPATH/bin:$PATH"; # g-install: do NOT edit, see https://github.com/stefanmaric/g
+export GOPATH="$HOME/go"; export GOROOT="$HOME/.go"; export PATH="$GOPATH/bin:$PATH"; # g-install: do NOT edit, see https://github.com/stefanmaric/g
 
 # MySQL
 PATH=$PATH:/usr/local/mysql/bin/
