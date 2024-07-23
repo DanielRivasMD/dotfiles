8ead6134255deb735e6c9733ef252e902aeda63a
DanielRivasMD <danielrivasmd@gmail.com>
2024-07-23 12:02:18 -0600

    update go install

--- install.sh
+++ install.sh
@@ -64,8 +64,16 @@ curl -sSL https://git.io/g-install | sh -s
 
 ####################################################################################################
 
-# install golang binaries
-# TODO: finish installation
+# language server
+go install golang.org/x/tools/gopls@latest
+go install github.com/go-delve/delve/cmd/dlv@latest
+go install github.com/nametake/golangci-lint-langserver@latest
+
+# binaries
+go install github.com/jesseduffield/lazygit@latest
+go install github.com/jesseduffield/lazydocker@latest
+go install github.com/jesseduffield/lazynpm@latest
+go install github.com/itchyny/mmv/cmd/mmv@latest
 
 ####################################################################################################
 
