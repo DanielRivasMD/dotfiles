28956064a895cd01eab3b9c267828d68fbf7d4c1
DanielRivasMD <danielrivasmd@gmail.com>
2024-07-31 21:06:16 -0600

    draft go install

---
+++ .install/go.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+####################################################################################################
+
+# go version manager
+curl -sSL https://git.io/g-install | sh -s
+
+####################################################################################################
+
+# language server
+go install golang.org/x/tools/gopls@latest
+go install github.com/go-delve/delve/cmd/dlv@latest
+go install github.com/nametake/golangci-lint-langserver@latest
+
+# binaries
+go install github.com/zyedidia/eget@latest
+go install github.com/jesseduffield/lazygit@latest
+go install github.com/jesseduffield/lazydocker@latest
+go install github.com/jesseduffield/lazynpm@latest
+go install github.com/itchyny/mmv/cmd/mmv@latest
+go install github.com/itchyny/fillin@latest
+
+####################################################################################################
+
+# # static binaries
+# eget zyedidia/micro
+
+####################################################################################################
