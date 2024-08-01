a64b6900c75fd7ff4a31fb3d159073d17f53fce3
DanielRivasMD <danielrivasmd@gmail.com>
2024-07-31 21:05:47 -0600

    segregate install

--- install.sh
+++ install.sh
@@ -2,102 +2,47 @@
 ####################################################################################################
 
 # setup
-# TODO: perhaps make it from current path
-dotDir="$HOME/Factorem/dotfiles"
 archDir="$HOME/.archive"
-setupDir="${archDir}/.just/.setup"
+installDir="${archDir}/.install"
 
 ####################################################################################################
 
-# TODO: include logging
 # create log directory
-if [ ! -d "${dotDir}/log" ]
+echo 'Checking log directory...'
+if [ ! -d "$(pwd)/log" ]
 then
-  mkdir "${dotDir}/log"
+  echo 'Creating log directory...'
+  mkdir "$(pwd)/log"
 fi
 
 # link directory
-ln -svf "${dotDir}" "${archDir}"
+echo 'Linking archive...'
+ln -svf "$(pwd)" "${archDir}"
 
 ####################################################################################################
 
-# install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# homebrew
+source "${installDir}/brew.sh"
 
-####################################################################################################
+# clojure
+source "${installDir}/clojure.sh"
 
-# install formulae
-while read item
-do
-  brew install --formulae ${item} 1>> "${archDir}/log/brew_formulae_out.txt" 2>> "${archDir}/log/brew_formulae_err.txt"
-done < "${setupDir}/brew_formulae.txt"
+# go
+source "${installDir}/go.sh"
+
+# julia
+source "${installDir}/julia.sh"
+
+# R
+source "${installDir}/R.sh"
+
+# rust & cargo
+source "${installDir}/rust.sh"
 
 
-# install casks
-while read item
-do
-  brew install --casks "${item}" 1>> "${archDir}/log/brew_cask_out.txt" 2>> "${archDir}/log/brew_cask_err.txt"
-done < "${setupDir}/brew_casks.txt"
 
-####################################################################################################
-
-# install rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-# reload shell
-source $HOME/.zshrc
-
-####################################################################################################
-
-# install rust binaries
-while read item
-do
-  cargo install ${item} 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
-done < "${setupDir}/cargo.txt"
-
-####################################################################################################
-
-# cargo install --locked lazycli
-
-####################################################################################################
-
-# go version manager
-curl -sSL https://git.io/g-install | sh -s
-
-####################################################################################################
-
-# language server
-go install golang.org/x/tools/gopls@latest
-go install github.com/go-delve/delve/cmd/dlv@latest
-go install github.com/nametake/golangci-lint-langserver@latest
-
-# binaries
-go install github.com/zyedidia/eget@latest
-go install github.com/jesseduffield/lazygit@latest
-go install github.com/jesseduffield/lazydocker@latest
-go install github.com/jesseduffield/lazynpm@latest
-go install github.com/itchyny/mmv/cmd/mmv@latest
-go install github.com/itchyny/fillin@latest
-
-####################################################################################################
-
-eget zyedidia/micro
-
-####################################################################################################
-
-# TODO: download R packages
-
-# # install R packages
-# R CMD INSTALL /Users/drivas/bin/Rpack/colorout_1.2-1.tar.gz
-# R CMD INSTALL /Users/drivas/bin/Rpack/SW_3.2.tar.gz
-
-####################################################################################################
-
-# brew tap r-lib/rig
-# brew install --cask rig
-# brew install yqrashawn/goku/goku
 # brew install fzf
-
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/weavejester/cljfmt/HEAD/install.sh)"
+# brew install gh
+# brew install hub
 
 ####################################################################################################
