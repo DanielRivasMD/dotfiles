16467e50de124ef1c2bb5452a14c3743e0b79544
DanielRivasMD <danielrivasmd@gmail.com>
2024-07-31 21:06:03 -0600

    draft brew install

---
+++ .install/brew.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+####################################################################################################
+
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+####################################################################################################
+
+# install formulae
+while read item
+do
+  brew install --formulae ${item} 1>> "${archDir}/log/brew_formulae_out.txt" 2>> "${archDir}/log/brew_formulae_err.txt"
+done < "${setupDir}/brew_formulae.txt"
+
+
+# install casks
+while read item
+do
+  brew install --casks "${item}" 1>> "${archDir}/log/brew_cask_out.txt" 2>> "${archDir}/log/brew_cask_err.txt"
+done < "${setupDir}/brew_casks.txt"
+
+####################################################################################################
+
+####################################################################################################
