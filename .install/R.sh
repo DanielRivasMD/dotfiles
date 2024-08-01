c926731b81a48cb9e9ce9cd3795efb582c59936c
DanielRivasMD <danielrivasmd@gmail.com>
2024-07-31 21:05:55 -0600

    draft R install

---
+++ .install/R.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+####################################################################################################
+
+# install R package manager
+brew tap r-lib/rig
+brew install --cask rig
+
+####################################################################################################
+
+# TODO: download R packages
+
+# # install R packages
+# R CMD INSTALL /Users/drivas/bin/Rpack/colorout_1.2-1.tar.gz
+# R CMD INSTALL /Users/drivas/bin/Rpack/SW_3.2.tar.gz
+
+####################################################################################################
+
+# install R packages from shell
+
+# TODO: re-write script into loop
+
+####################################################################################################
+
+# # install remote packages
+# install.packages('magrittr')
+# install.packages('pacman')
+# install.packages('startup')
+# install.packages('tidyverse')
+
+####################################################################################################
