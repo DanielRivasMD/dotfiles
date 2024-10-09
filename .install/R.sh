32fa1a2670837d33cf57c90f3ad39dc127f1b140
DanielRivasMD <danielrivasmd@gmail.com>
2024-10-09 15:10:59 +0200

    update install R

--- .install/R.sh
+++ .install/R.sh
@@ -8,23 +8,24 @@ brew install --cask rig
 ####################################################################################################
 
 # TODO: download R packages
-
 # # install R packages
 # R CMD INSTALL /Users/drivas/bin/Rpack/colorout_1.2-1.tar.gz
 # R CMD INSTALL /Users/drivas/bin/Rpack/SW_3.2.tar.gz
 
 ####################################################################################################
 
-# install R packages from shell
 
 # TODO: re-write script into loop
+packages <- c(
+  'magrittr',
+  'pacman',
+  'startup',
+  'tidyverse'
+)
 
-####################################################################################################
-
-# # install remote packages
-# install.packages('magrittr')
-# install.packages('pacman')
-# install.packages('startup')
-# install.packages('tidyverse')
+# install R packages from shell
+for (pk in packages) {
+  install.packages(pk)
+}
 
 ####################################################################################################
