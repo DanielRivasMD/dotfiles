04ccda1ca6041e71443dbc83e3ef0e3db3d1ec89
DanielRivasMD <danielrivasmd@gmail.com>
2024-10-26 15:53:06 -0600

    use mbombo helix forge

--- in-silico/.forge/helix.sh
+++ in-silico/.forge/helix.sh
@@ -11,20 +11,19 @@ _forge() {
   # create temporary files
   for type in normal insert select
   do
-    awk -v mode="${type}_mode" '{gsub("MODE", mode); print $0}' "${moded}/common.toml" > "${helix}/.${type}.tmp"
+    mbombo forge --path "${moded}" --out "${helix}/.${type}.tmp" --files "common.toml" --old MODE --new "${type}_mode"
   done
 
   # concatenate
-  cat \
-    "${moded}/theme.toml" \
-    "${moded}/editor.toml" \
-    "${moded}/normal.toml" \
-    "${helix}/.normal.tmp" \
-    "${moded}/insert.toml" \
-    "${helix}/.insert.tmp" \
-    "${moded}/select.toml" \
-    "${helix}/.select.tmp" \
-    > "${helix}/config.toml"
+  mbombo forge --path "${moded}" --out "${helix}/config.toml" \
+    --files "theme.toml" \
+    --files "editor.toml" \
+    --files "normal.toml" \
+    --files "../.normal.tmp" \
+    --files "insert.toml" \
+    --files "../.insert.tmp" \
+    --files "select.toml" \
+    --files "../.select.tmp"
 
   # purge temporary files
   rm "${helix}/.normal.tmp"
