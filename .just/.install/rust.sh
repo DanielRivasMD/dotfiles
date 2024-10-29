f53d9ad1491cfb2a0331261411f1db1f3898ed56
DanielRivasMD <danielrivasmd@gmail.com>
2024-10-29 17:21:44 -0600

    update rust install

--- .just/.install/rust.sh
+++ .just/.install/rust.sh
@@ -12,6 +12,10 @@ source $HOME/.zshrc
 # install deno
 curl -fsSL https://deno.land/install.sh | sh
 
+# install deployctl
+deno install -A jsr:@deno/deployctl --global
+
+
 ####################################################################################################
 # install rust binaries
 ####################################################################################################
@@ -19,23 +23,13 @@ curl -fsSL https://deno.land/install.sh | sh
 # cargo add ratatui
 # atuin
 # curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
-cargo install atuin 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
-
-# bacon
-cargo install --locked bacon 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
-
-# bartib
-cargo install bartib 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
-
-# bat
-cargo install --locked bat 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
-
-# bottom
-cargo install bottom --locked 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
-
-# broot
-cargo install --locked --features clipboard broot 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
-
+{ \
+  cargo install atuin;                                                         # atuin
+  cargo install --locked bacon;                                                # bacon
+  cargo install bartib;                                                        # bartib
+  cargo install --locked bat;                                                  # bat
+  cargo install bottom --locked;                                               # bottom
+  cargo install --locked --features clipboard broot;                           # broot
 # cargo-benchcmp
 # cargo-binstall
 # cargo-cache
@@ -136,110 +130,45 @@ cargo install --locked --features clipboard broot 1>> "${archDir}/log/cargo_out.
 # kondo
 
 
-# lazycli
-cargo install --locked lazycli
-
+  cargo install --locked lazycli;                                              # lazycli
 # lsd
 
 
 # mdcat
 
 
-# melody_cli
-cargo install melody_cli 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
+  cargo install melody_cli;                                                    # melody_cli
+  cargo install mgit;                                                          # mgit
+  cargo install monolith;                                                      # monolith
+  brew install navi;                                                           # navi
+  cargo install nomino;                                                        # nomino
+  cargo install nu;                                                            # nu
+  cargo install onefetch;                                                      # onefetch
+  cargo install onehistory;                                                    # onehistory
+  cargo install pastel;                                                        # pastel
+  cargo install print_key;                                                     # print_key
+  cargo install procs;                                                         # procs
+  cargo install pueue --locked;                                                # pueue
+  cargo install ripgrep;                                                       # ripgrep
+  cargo install ripgrep_all --locked;                                          # ripgrep_all
+  cargo install samesame;                                                      # samesame
+  cargo install sccache --locked;                                              # sccache
+  cargo install sd;                                                            # sd
+  cargo install sheldon --locked;                                              # sheldon
+  cargo install spotify-tui;                                                   # spotify-tui
+  cargo install starship --locked;                                             # starship
+  cargo install tauri-cli;                                                     # tauri
+  cargo install tealdeer;                                                      # tealdeer
+  cargo install tock;                                                          # tock
+  cargo install --git https://github.com/XAMPPRocky/tokei.git tokei;           # tokei
+  cargo install viu;                                                           # viu
+  cargo install watchexec-cli --locked;                                        # watchexec-cli
+  cargo install wordcrab;                                                      # wordcrab
+  cargo install xcp;                                                           # xcp
+  cargo install xsv;                                                           # xsv
+  cargo install --locked yazi-fm yazi-cli;                                     # yazi
+  cargo install zellij --locked;                                               # zellij
+  cargo install zoxide --locked;                                               # zoxide
+} 1>> "${archDir}/log/cargo_out.tt" 2>> "${archDir}/log/cargo_err.txt          "
 
-# mgit
-cargo install mgit 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
-
-# monolith
-cargo install monolith 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
-
-# navi
-# TODO: update
-# brew install navi 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
-
-# nomino
-cargo install nomino 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
-
-# nu
-cargo install nu 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
-
-# onefetch
-cargo install onefetch 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
-
-# onehistory
-cargo install onehistory 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
-
-# pastel
-cargo install pastel 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
-
-# print_key
-cargo install print_key 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
-
-# procs
-cargo install procs 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
-
-# pueue
-cargo install pueue --locked 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
-
-# ripgrep
-cargo install ripgrep 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
-
-# ripgrep_all
-cargo install ripgrep_all --locked 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
-
-# samesame
-cargo install samesame 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
-
-# sccache
-cargo install sccache --locked
-
-# sd
-cargo install sd
-
-# sheldon
-cargo install sheldon --locked
-
-# spotify-tui
-cargo install spotify-tui
-
-# starship
-cargo install starship --locked
-
-# tauri
-cargo install tauri-cli 1>> "${archDir}/log/cargo_out.txt" 2>> "${archDir}/log/cargo_err.txt"
-
-# tealdeer
-cargo install tealdeer
-
-# tock
-cargo install tock
-
-# tokei
-cargo install --git https://github.com/XAMPPRocky/tokei.git tokei
-
-# viu
-cargo install viu
-
-# watchexec-cli
-cargo install watchexec-cli --locked
-
-# wordcrab
-cargo install wordcrab
-
-# xcp
-cargo install xcp
-
-# xsv
-cargo install xsv
-
-# yazi
-cargo install --locked yazi-fm yazi-cli
-
-# zellij
-cargo install zellij --locked
-
-# zoxide
-cargo install zoxide --locked
-
-####################################################################################################
+####################################################################################################\ 
