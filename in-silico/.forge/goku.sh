6c52123da7e067b6b420ccd9610a42ab8798342c
DanielRivasMD <danielrivasmd@gmail.com>
2024-10-31 17:31:29 -0600

    update goku forge

--- in-silico/.forge/goku.sh
+++ in-silico/.forge/goku.sh
@@ -36,30 +36,31 @@ HEAD
     --files "../.profile.tmp" \
     --files "profile/profile.edn" \
     --files "../.main.tmp" \
+    --files "apps/alacritty.edn" \
     --files "apps/browser.edn" \
     --files "apps/editor.edn" \
     --files "apps/lapce.edn" \
     --files "apps/mail.edn" \
     --files "apps/mouse.edn" \
     --files "apps/zed.edn" \
-    --files "apps/alacritty.edn" \
+    --files "apps/zellij.edn" \
     --files "simple/bs.edn" \
-    --files "simple/lcmd.edn" \
+    --files "simple/cbracket.edn" \
     --files "simple/cc.edn" \
     --files "simple/esc.edn" \
     --files "simple/function.edn" \
     --files "simple/joker.edn" \
-    --files "simple/rshift.edn" \
+    --files "simple/lcmd.edn" \
     --files "simple/lctl.edn" \
     --files "simple/lopt.edn" \
     --files "simple/lshift.edn" \
+    --files "simple/obracket.edn" \
     --files "simple/patch.edn" \
-    --files "simple/q-layer.edn" \
     --files "simple/rcmd.edn" \
     --files "simple/rctl.edn" \
+    --files "simple/rshift.edn" \
     --files "simple/ropt.edn" \
     --files "simple/tab.edn" \
-    --files "apps/zellij.edn" \
     --files "profile/keys.edn" \
     --files "../.eof.tmp"
 
