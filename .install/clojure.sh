03028ee1e9f693ba0e811d7ef671af5579585812
DanielRivasMD <danielrivasmd@gmail.com>
2024-11-27 21:11:47 -0600

    update clojure install

--- .install/clojure.sh
+++ .install/clojure.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 ####################################################################################################
 
+# archive
+archDir="$HOME/.archive"
+
+####################################################################################################
+
 # install goku karabiner
-brew install yqrashawn/goku/goku
+brew install yqrashawn/goku/goku 1>> "${archDir}/log/clojure.out" 2>> "${archDir}/log/clojure.err"
 
 ####################################################################################################
 
 # clojure format
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/weavejester/cljfmt/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/weavejester/cljfmt/HEAD/install.sh)" 1>> "${archDir}/log/clojure.out" 2>> "${archDir}/log/clojure.err"
 
 ####################################################################################################
