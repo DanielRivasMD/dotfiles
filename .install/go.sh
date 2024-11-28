aa11fb92cebc4c6b005df4158de2200168df9273
DanielRivasMD <danielrivasmd@gmail.com>
2024-11-28 12:12:48 -0600

    update go install

--- .install/go.sh
+++ .install/go.sh
@@ -9,20 +9,20 @@ archDir="$HOME/.archive"
 # go version manager
 curl -sSL https://git.io/g-install | sh -s 1>> "${archDir}/log/go.out" 2>> "${archDir}/log/go.err"
 
-# ####################################################################################################
+####################################################################################################
 
-# # language server
-# go install golang.org/x/tools/gopls@latest
-# go install github.com/go-delve/delve/cmd/dlv@latest
-# go install github.com/nametake/golangci-lint-langserver@latest
+# language server
+go install golang.org/x/tools/gopls@latest
+go install github.com/go-delve/delve/cmd/dlv@latest
+go install github.com/nametake/golangci-lint-langserver@latest
 
-# # binaries
-# go install github.com/zyedidia/eget@latest
-# go install github.com/jesseduffield/lazygit@latest
-# go install github.com/jesseduffield/lazydocker@latest
-# go install github.com/jesseduffield/lazynpm@latest
-# go install github.com/itchyny/mmv/cmd/mmv@latest
-# go install github.com/itchyny/fillin@latest
-# go install github.com/spf13/cobra-cli@latest
+# binaries
+go install github.com/zyedidia/eget@latest
+go install github.com/jesseduffield/lazygit@latest
+go install github.com/jesseduffield/lazydocker@latest
+go install github.com/jesseduffield/lazynpm@latest
+go install github.com/itchyny/mmv/cmd/mmv@latest
+go install github.com/itchyny/fillin@latest
+go install github.com/spf13/cobra-cli@latest
 
-# ####################################################################################################
+####################################################################################################
