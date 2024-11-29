9a0bc98af08cf6c8fa2a03b62255d8f291ade180
DanielRivasMD <danielrivasmd@gmail.com>
2024-11-28 19:23:44 -0600

    update julia install

--- .install/julia.sh
+++ .install/julia.sh
@@ -7,22 +7,6 @@ brew install juliaup
 ####################################################################################################
 
 # install packages
-julia -e << TESTA
-packages = [
-  "OhMyREPL",
-  "Chain",
-  "Pipe",
-  "Revise",
-  "Debugger",
-  "UnicodePlots",
-  "DelimitedFiles",
-]
-
-using Pkg
-for pk in packages
-  @info pk
-  Pkg.add(pk)
-end
-TESTA
+julia --eval 'using Pkg; for pk in ["OhMyREPL", "Chain", "Pipe", "Revise", "Debugger", "UnicodePlots", "DelimitedFiles"] Pkg.add(pk) end'
 
 ####################################################################################################
