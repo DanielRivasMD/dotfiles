0460cfacc492a05f139c07817cf4f0cc2208133d
Daniel Rivas <danielrivasmd@gmail.com>
2025-01-26 20:12:13 -0600

    update R install

--- .install/R.sh
+++ .install/R.sh
@@ -17,8 +17,16 @@ R -e "install.packages(c('magrittr', 'pacman', 'startup', 'tidyverse'))"
 
 ####################################################################################################
 
-# # install R packages
-# R CMD INSTALL /Users/drivas/bin/Rpack/colorout_1.2-1.tar.gz
-# R CMD INSTALL /Users/drivas/bin/Rpack/SW_3.2.tar.gz
+# install R packages
+(
+  if [[ ! -d "${HOME}/Rpackage" ]]; then mkdir "${HOME}/Rpackage"; fi
+  cd "${HOME}/Rpackage" || exit
+
+  curl -O https://github.com/jalvesaq/colorout/releases/download/v1.3-2/colorout_1.3-2.tar.gz
+  R CMD INSTALL "${HOME}/RPackage/colorout_1.2-1.tar.gz"
+
+  curl -O https://github.com/DanielRivasMD/SW/archive/refs/tags/v3.2.tar.gz
+  R CMD INSTALL "${HOME}/RPackage/SW_3.2.tar.gz"
+)
 
 ####################################################################################################
