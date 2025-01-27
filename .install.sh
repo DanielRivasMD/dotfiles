2e27a666ca17ce2bb6bd3ff1a2af151cf9284302
Daniel Rivas <danielrivasmd@gmail.com>
2025-01-26 20:15:43 -0600

    update install run as subshells

--- .install.sh
+++ .install.sh
@@ -21,25 +21,26 @@ ln -svf "$(pwd)" "${archDir}"
 ####################################################################################################
 
 # homebrew
-source "${archDir}/.install/brew.sh"  1>> "${archDir}/log/brew.out" 2>> "${archDir}/log/brew.err"
+( source "${archDir}/.install/brew.sh"  1>> "${archDir}/log/brew.out" 2>> "${archDir}/log/brew.err" ) &
 
 # c++
-source "${archDir}/.install/c++.sh"  1>> "${archDir}/log/c++.out" 2>> "${archDir}/log/c++.err"
+( source "${archDir}/.install/c++.sh"  1>> "${archDir}/log/c++.out" 2>> "${archDir}/log/c++.err" ) &
 
 # clojure
-source "${archDir}/.install/clojure.sh" 1>> "${archDir}/log/clojure.out" 2>> "${archDir}/log/clojure.err"
+( source "${archDir}/.install/clojure.sh" 1>> "${archDir}/log/clojure.out" 2>> "${archDir}/log/clojure.err" ) &
 
 # go
-source "${archDir}/.install/go.sh" 1>> "${archDir}/log/go.out" 2>> "${archDir}/log/go.err"
+( source "${archDir}/.install/go.sh" 1>> "${archDir}/log/go.out" 2>> "${archDir}/log/go.err" ) &
 
 # julia
-source "${archDir}/.install/julia.sh" 1>> "${archDir}/log/julia.out" 2>> "${archDir}/log/julia.err"
+( source "${archDir}/.install/julia.sh" 1>> "${archDir}/log/julia.out" 2>> "${archDir}/log/julia.err" ) &
 
 # R
-source "${archDir}/.install/R.sh" 1>> "${archDir}/log/R.out" 2>> "${archDir}/log/R.err"
+( source "${archDir}/.install/R.sh" 1>> "${archDir}/log/R.out" 2>> "${archDir}/log/R.err" ) &
 
 # rust & cargo
-source "${archDir}/.install/rust.sh" 1>> "${archDir}/log/rust.out" 2>> "${archDir}/log/rust.err"
+( source "${archDir}/.install/rust.sh" 1>> "${archDir}/log/rust.out" 2>> "${archDir}/log/rust.err" ) &
+
 ####################################################################################################
 # ergo
 ####################################################################################################
