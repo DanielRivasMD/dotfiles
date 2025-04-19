46138caf904693002c17328983499a7ec5fdcbbc
Daniel Rivas <danielrivasmd@gmail.com>
2025-04-19 15:18:50 -0600

    add zellij float function

--- ex-situ/shell/zsh/zsh_zellij.sh
+++ ex-situ/shell/zsh/zsh_zellij.sh
@@ -16,6 +16,7 @@ fi
 
 ####################################################################################################
 
+# rename tab with current directory
 # function current_dir() {
 # 	local current_dir=$PWD
 # 	if [[ $current_dir == $HOME ]]; then
@@ -52,5 +53,44 @@ fi
 # 	add-zsh-hook precmd set_tab_to_working_dir
 # 	add-zsh-hook preexec set_tab_to_command_line
 # fi
+
+####################################################################################################
+
+# function: run_zellij_bat
+# description: run zellij with bat
+# arguments:
+#   $1: command
+#   $2: filename
+
+zf() {
+  if [ -z "$1" ]; then
+    echo "Error: Command argument is missing."
+    return 1
+  fi
+  cmd=$(printf "%s" "$1") # store command
+
+  if [ $# -eq 1 ]; then #check if only one argument is passed
+    echo "Error: At least one filename argument is required."
+    return 1
+  fi
+
+  # iterate through arguments
+  shift # remove first argument
+
+  file=$(printf "%s" "$1") # sanitize
+  if [ ! -f "$file" ]; then
+    echo "Error: File '$file' not found."
+    return 1
+  fi
+
+  zellij run \
+    --name float \
+    --floating \
+    --height 100 \
+    --width 100 \
+    --x 100 \
+    --y 0 \
+    -- "$cmd" "$file"
+}
 
 ####################################################################################################
