2e1384705fd2b709a54a34bb2ad997832c6cdd28
Daniel Rivas <danielrivasmd@gmail.com>
2025-07-10 10:29:10 +0200

    deprecate cheat

--- in-situ/cheat/cheatsheets/bash/bashStringSubstitution.sh
+++
@@ -1,9 +0,0 @@
-
-# string substitution
-newVar=${oldVar/<pattern>/<replace>}
-
-# global string substitution
-newVar=${oldVar//<pattern>/<replace>}
-
-# applied to blank file names
-for f in *\ *; do mv "$f" "${f// /_}"; done
--- in-situ/cheat/cheatsheets/rust/rustArgs.rs
+++
@@ -1,7 +0,0 @@
----
-syntax: rust
----
-
-// collect arguments
-let args: Vec<String> = env::args().collect();
-let command = args[1].clone();
