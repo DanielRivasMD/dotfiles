ec9058dc523e7183a25237e6c59f213351b992ea
Daniel Rivas <danielrivasmd@gmail.com>
2025-07-23 13:47:58 +0200

    update uv install

--- in-silico/uv.sh
+++ in-silico/uv.sh
@@ -8,11 +8,11 @@ brew install uv
 # Install Binaries
 ####################################################################################################
 
-uv tool install gcalcli                # Google Calendar CLI
-uv tool install bpython                # Fancy Python REPL
-uv tool install litecli                # SQLite client with UX improvements
-uv tool install mycli                  # MySQL CLI with autocompletion
-uv tool install pgcli                  # PostgreSQL CLI with features
-uv tool install ruff                   # Python linter & formatter
+uv tool install gcalcli      # Google Calendar CLI
+uv tool install bpython      # Fancy Python REPL
+uv tool install litecli      # SQLite client with UX improvements
+uv tool install mycli        # MySQL CLI with autocompletion
+uv tool install pgcli        # PostgreSQL CLI with features
+uv tool install ruff         # Python linter & formatter
 
 ####################################################################################################
