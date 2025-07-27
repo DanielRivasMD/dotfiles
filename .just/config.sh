1d6ed743264ea5b5691af0e1507c0d2dd86206d1
Daniel Rivas <danielrivasmd@gmail.com>
2025-07-27 18:51:50 +0200

    rfac: update config just

--- .just/config.sh
+++ .just/config.sh
@@ -1,110 +1,70 @@
 ####################################################################################################
+# Helpers
+####################################################################################################
+
+sep() {
+  local red='\033[0;31m'
+  local nc='\033[0m'
+  echo "${red}====================================================================================================${nc}"
+}
+
+echo_header() {
+  local green='\033[1;32m'
+  local nc='\033[0m'
+  local header=$1
+  echo ''
+  echo "${green}${header}${nc}"
+}
+
+# link_config <source> <destination> <label>
+link_config() {
+  local byellow='\033[1;33m'
+  local bblue='\033[1;34m'
+  local nc='\033[0m'
+  local src=$1 dst=$2 out=${3:-}
+  ln -sf "$src" "$dst" && echo "Linked ${byellow}=>${nc} ${bblue}${out}${nc}"
+}
+
+# generate_completion <command> <output-filename>
+generate_completion() {
+  local byellow='\033[1;33m'
+  local bcyan='\033[1;36m'
+  local nc='\033[0m'
+  local cmd=$1 out=$2
+  eval "$cmd" > "${zshcomp}/${out}" && echo "Installed completion ${byellow}=>${nc} ${bcyan}${out}${nc}"
+}
+
+####################################################################################################
 # config
 ####################################################################################################
 
 # home
 export home="${HOME}"
 export config="${home}/.config"
-export archive="${home}/.archive"
-export in_situ="${archive}/in-situ"
-export ex_situ="${archive}/ex-situ"
-export in_silico="${archive}/in-silico"
-export forked="${home}/Forked"
-export completion="${home}/.completion"
-export linked="${home}/Linked"
-export appSupport="${home}/Library/Application Support"
 export localShare="${home}/.local/share"
-export completionArch="${archive}/completions"
+export completionHome="${home}/.completion"
+export forkedHome="${home}/Forked"
+export linkedHome="${home}/Linked"
+export appSupport="${home}/Library/Application Support"
 
-####################################################################################################
+# dotfiles
+dotfiles=$(git rev-parse --show-toplevel)
+export in_situ="${dotfiles}/in-situ"
+export ex_situ="${dotfiles}/ex-situ"
+export in_silico="${dotfiles}/in-silico"
+export completionDot="${dotfiles}/completions"
+
 # in-situ
-####################################################################################################
+export naviAppS="${appSupport}/navi/"
+export espansoAppS="${appSupport}/espanso"
+export halpAppS="${appSupport}/halp"
+export lazygitAppS="${appSupport}/jesseduffield/lazygit"
+export lazynpmAppS="${appSupport}/jesseduffield/lazynpm"
+export lazycliAppS="${appSupport}/lazycli"
+export lazydockerAppS="${appSupport}/lazydocker"
 
-# git files
-export git="${in_situ}/git"
-export gitconfig="${git}/gitconfig"
-export gitignore="${git}/gitignore_global"
-
-# rc config
-export mplayer="${in_situ}/mplayer/mplayer"
-
-# toml files
-export procs="${in_situ}/procs/procs.toml"
-
-# config files
-export atuin="${in_situ}/atuin"
-export alacritty="${in_situ}/alacritty"
-export bottom="${in_situ}/bottom"
-export joplin="${in_situ}/joplin"
-
-export gh="${in_situ}/gh"
-export spotify="${in_situ}/spotify"
-export saiyajin="${home}/.saiyajin"
-export karabiner="${saiyajin}/karabiner"
-export frag="${saiyajin}/frag"
-export fapps="${frag}/apps"
-export fprofile="${frag}/profile"
-export fmode="${frag}/mode"
-export fsimple="${frag}/simple"
-
-# config dirs
-export zellij="${in_situ}/zellij"
-
-# distant
-export julia="${in_situ}/julia"
-export sshConfig="${in_situ}/ssh/config"
-export lazycli="${in_situ}/lazycli"
-export lazycliConf="${appSupport}/lazycli"
-export lazygit="${in_situ}/lazygit"
-export lazygitConf="${appSupport}/jesseduffield/lazygit"
-export halp="${in_situ}/halp"
-export halpConf="${appSupport}/halp"
-
-# broot
-export broot="${in_situ}/broot"
-export brootConf="${home}/.config/broot"
-
-# espanso
-export espanso="${in_situ}/espanso"
-export espansoConfig="${espanso}/config"
-export espansoMatch="${espanso}/match"
-export espansoConf="${appSupport}/espanso"
-
-# navi
-export navi="${in_situ}/navi"
-export naviConf="${appSupport}/navi/"
-
-####################################################################################################
 # ex-situ
-####################################################################################################
-
-# @HOME
-export screen="${ex_situ}/screen"
-export mycli="${ex_situ}/mycli"
-
-# @config
-export bpython="${ex_situ}/bpython"
-export helix="${ex_situ}/helix"
-export moded="${helix}/modes"
-export litecli="${ex_situ}/litecli"
-export lsd="${ex_situ}/lsd"
-export micro="${ex_situ}/micro"
-export sheldon="${ex_situ}/sheldon"
-export starship="${ex_situ}/starship"
-
-# shell
-export shell="${ex_situ}/shell"
-export term="${shell}/term"
-
-# bash
-export bash="${shell}/bash"
-
-# zsh
-export zsh="${shell}/zsh"
-
-# nushell
-export nushell="${shell}/nu"
-export nushellConf="${appSupport}/nushell"
+export nuAppS="${appSupport}/nushell"
 
 ####################################################################################################
 # ergo
@@ -114,23 +74,11 @@ export nushellConf="${appSupport}/nushell"
 export zshcomp="${config}/zsh_completion"
 
 ####################################################################################################
-
 # remote
-export remoteBin="${home}/RemoteBin"
-export homeRemote="/home/drivas"
-export ex_situRemote="${homeRemote}/.archive/ex-situ"
-export shellRemote="${ex_situRemote}/shell"
-export termRemote="${shellRemote}/term"
-export bashRemote="${shellRemote}/bash"
-export zshRemote="${shellRemote}/zsh"
+####################################################################################################
 
 # pawsey
 export pawseyID="drivas@topaz.pawsey.org.au"
 export softwarePawsey="/scratch/pawsey0263/drivas/software"
-
-####################################################################################################
-
-# load colors
-source "${archive}/.just/colors.sh"
 
 ####################################################################################################
