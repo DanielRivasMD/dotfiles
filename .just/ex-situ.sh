edf22c2670d106957f6cb901cbcdcf75f40c930b
Daniel Rivas <danielrivasmd@gmail.com>
2025-07-27 18:52:00 +0200

    rfac: update ex situ

--- .just/ex-situ.sh
+++ .just/ex-situ.sh
@@ -1,7 +1,21 @@
 ####################################################################################################
 
 # config
-source "${HOME}/.archive/.just/config.sh"
+source "${PWD}/.just/config.sh"
+
+####################################################################################################
+
+for entry in "$ex_situ"/*; do
+  [[ -d "$entry" ]] || continue
+  name=${entry##*/}
+  eval "export $name=\"$entry\""
+done
+
+for entry in "$shell"/*; do
+  [[ -d "$entry" ]] || continue
+  name=${entry##*/}
+  eval "export $name=\"$entry\""
+done
 
 ####################################################################################################
 # in effigie
@@ -20,7 +34,6 @@ sep
 
 # @config
 echo_header "@config"
-
 tools=( 
   bpython
   helix
@@ -34,7 +47,6 @@ for tool in "${tools[@]}"; do
   if [[ -e "${config}/${tool}" ]]; then rm -rf "${config}/${tool}"; fi
   link_config "${!tool}" "${config}/" "${tool}"
 done
-
 sep
 
 
@@ -43,29 +55,26 @@ sep
 ####################################################################################################
 
 echo_header "term & shell"
-
-# Term “osx_profile.sh” → ~/.profile
-link_config "${term}/osx_profile.sh"   "${home}/.profile"                      "term profile"
+link_config "${term}/osx_profile.sh" "${home}/.profile" "term profile"
 
 # Bash
 echo '' && mkdir -p "${home}/.bash" && echo "Created ~/.bash/"
-link_config "${bash}/osx_bashrc.sh"    "${home}/.bashrc"                       "bashrc"
-link_config "${bash}/bash_just.sh"     "${home}/.bash"                         "bash just"
-link_config "${bash}/bash_navi.sh"     "${home}/.bash"                         "bash navi"
+link_config "${bash}/osx_bashrc.sh" "${home}/.bashrc" "bashrc"
+link_config "${bash}/bash_just.sh" "${home}/.bash" "bash just"
+link_config "${bash}/bash_navi.sh" "${home}/.bash" "bash navi"
 
 # Zsh
 echo '' && mkdir -p "${home}/.zsh" && echo "Created ~/.zsh/"
-link_config "${zsh}/osx_zshenv.sh"     "${home}/.zshenv"                       "zshenv"
-link_config "${zsh}/osx_zshrc.sh"      "${home}/.zsh/.zshrc"                   "zshrc"
-link_config "${zsh}/zsh_navi.sh"       "${home}/.zsh"                          "zsh navi"
-link_config "${zsh}/zsh_zellij.sh"     "${home}/.zsh"                          "zsh zellij"
-link_config "${zsh}/zsh_zoxide.sh"     "${home}/.zsh"                          "zsh zoxide"
+link_config "${zsh}/osx_zshenv.sh" "${home}/.zshenv" "zshenv"
+link_config "${zsh}/osx_zshrc.sh" "${home}/.zsh/.zshrc" "zshrc"
+link_config "${zsh}/zsh_navi.sh" "${home}/.zsh" "zsh navi"
+link_config "${zsh}/zsh_zellij.sh" "${home}/.zsh" "zsh zellij"
+link_config "${zsh}/zsh_zoxide.sh" "${home}/.zsh" "zsh zoxide"
 
 # Nushell
 echo ''
-link_config "${nushell}/env.nu"        "${nushellConf}/env.nu"                 "nushell env"
-link_config "${nushell}/config.nu"     "${nushellConf}/config.nu"              "nushell config"
-
+link_config "${nu}/env.nu" "${nuAppS}/env.nu" "nushell env"
+link_config "${nu}/config.nu" "${nuAppS}/config.nu" "nushell config"
 sep
 
 
@@ -74,25 +83,23 @@ sep
 ####################################################################################################
 
 echo_header "completions"
-
 mkdir -p "${zshcomp}" && echo "Created ~/.config/zsh_completion/"
-generate_completion          "juliaup completions zsh"        "_juliaup"
-generate_completion          "sheldon completions --shell zsh"      "_sheldon"
+generate_completion "juliaup completions zsh" "_juliaup"
+generate_completion "sheldon completions --shell zsh" "_sheldon"
 
 echo ''
-generate_completion          "babel completion zsh"        "_babel"
-generate_completion          "bender completion zsh"       "_bender"
-generate_completion          "chess completion zsh"        "_chess"
-generate_completion          "cerberus completion zsh"     "_cerberus"
-generate_completion          "gorgon completion zsh"       "_gorgon"
-generate_completion          "hypnos completion zsh"       "_hypnos"
-generate_completion          "mimir completion zsh"        "_mimir"
-generate_completion          "lilith completion zsh"       "_lilith"
-generate_completion          "lou completion zsh"          "_lou"
-generate_completion          "mbombo completion zsh"       "_mbombo"
-generate_completion          "minerva completion zsh"      "_minerva"
-generate_completion          "sisu completion zsh"         "_sisu"
-
+generate_completion "babel completion zsh" "_babel"
+generate_completion "bender completion zsh" "_bender"
+generate_completion "chess completion zsh" "_chess"
+generate_completion "cerberus completion zsh" "_cerberus"
+generate_completion "gorgon completion zsh" "_gorgon"
+generate_completion "hypnos completion zsh" "_hypnos"
+generate_completion "mimir completion zsh" "_mimir"
+generate_completion "lilith completion zsh" "_lilith"
+generate_completion "lou completion zsh" "_lou"
+generate_completion "mbombo completion zsh" "_mbombo"
+generate_completion "minerva completion zsh" "_minerva"
+generate_completion "sisu completion zsh" "_sisu"
 sep
 
 
