ce00c8a7c187749ada50aba56c5bc056545412e1
Daniel Rivas <danielrivasmd@gmail.com>
2025-10-04 22:32:48 +0300

    update julia install

--- in-silico/julia.sh
+++ in-silico/julia.sh
@@ -1,28 +1,4 @@
 ####################################################################################################
-# Install Julia Package Manager
-####################################################################################################
-
-os="$(uname -s)"
-case "$os" in
-  Darwin)
-    echo "Detected macOS – installing Julia via Homebrew juliaup..."
-    brew install juliaup
-    ;;
-  Linux)
-    if grep -qi ubuntu /etc/os-release; then
-      echo "Detected Ubuntu – installing Julia via apt..."
-      # install juliaup (official installer)
-      curl -fsSL https://install.julialang.org | sh
-    else
-      echo "Linux detected but not Ubuntu – please install Julia manually."
-    fi
-    ;;
-  *)
-    echo "Unsupported OS: $os" >&2
-    ;;
-esac
-
-####################################################################################################
 # Install Julia Packages
 ####################################################################################################
 
@@ -36,7 +12,6 @@ Pkg.add([
   "Debugger",
   "UnicodePlots",
   "DelimitedFiles"
-])
-'
+])'
 
 ####################################################################################################
