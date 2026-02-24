a2fcf4770f5fab350ac649f903d7803515a52bda
Daniel Rivas <danielrivasmd@gmail.com>
2026-02-24 06:59:10 +0200

    update & format go in silico

--- in-silico/go.sh
+++ in-silico/go.sh
@@ -3,9 +3,9 @@
 ####################################################################################################
 
 TOOLS=(
-  golang.org/x/tools/gopls                                 # Go language server
-  github.com/go-delve/delve/cmd/dlv                        # Go debugger
-  github.com/nametake/golangci-lint-langserver             # Lint language server
+  golang.org/x/tools/gopls                     # Go language server
+  github.com/go-delve/delve/cmd/dlv            # Go debugger
+  github.com/nametake/golangci-lint-langserver # Lint language server
 )
 
 for tool in "${TOOLS[@]}"; do
@@ -18,17 +18,18 @@ done
 ####################################################################################################
 
 BINARIES=(
-  github.com/air-verse/air                                 # live reload Go apps
-  github.com/Gelio/go-global-update                        # global Go updater
-  github.com/golangci/golangci-lint/cmd/golangci-lint      # lint tool
-  github.com/itchyny/mmv/cmd/mmv                           # mass renamer
-  github.com/jesseduffield/lazydocker                      # Docker UI
-  github.com/jesseduffield/lazygit                         # Git UI
-  github.com/jesseduffield/lazynpm                         # npm UI
-  github.com/mikefarah/yq/v4                               # YAML processor
-  github.com/spf13/cobra-cli                               # CLI generator
-  github.com/sqls-server/sqls                              # SQL language server
-  github.com/zyedidia/eget                                 # binary installer
+  mvdan.cc/sh/v3/cmd/shfmt                            # shell formatter
+  github.com/air-verse/air                            # live reload Go apps
+  github.com/Gelio/go-global-update                   # global Go updater
+  github.com/golangci/golangci-lint/cmd/golangci-lint # lint tool
+  github.com/itchyny/mmv/cmd/mmv                      # mass renamer
+  github.com/jesseduffield/lazydocker                 # Docker UI
+  github.com/jesseduffield/lazygit                    # Git UI
+  github.com/jesseduffield/lazynpm                    # npm UI
+  github.com/mikefarah/yq/v4                          # YAML processor
+  github.com/spf13/cobra-cli                          # CLI generator
+  github.com/sqls-server/sqls                         # SQL language server
+  github.com/zyedidia/eget                            # binary installer
   github.com/aarondl/sqlboiler/v4
   github.com/aarondl/sqlboiler-sqlite3
 )
