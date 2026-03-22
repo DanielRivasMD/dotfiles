120ab7ff4b83b3337bfce5d1278e23d94672f972
Daniel Rivas <danielrivasmd@gmail.com>
2026-03-22 14:22:48 +0200

    update profile bash

--- ex-situ/shell/bash/osx_bash_profile.sh
+++ ex-situ/shell/bash/osx_bash_profile.sh
@@ -43,22 +43,11 @@ export BASH_SILENCE_DEPRECATION_WARNING=1
 # Tools & Plugins
 ####################################################################################################
 
-# Atuin (History Manager)
 command -v atuin >/dev/null && {
   export ATUIN_NOBIND="true"
   eval "$(atuin init bash)"
 }
 
-# Broot (File Navigator)
-[[ -f "$HOME/.config/broot/launcher/bash/br" ]] && source "$HOME/.config/broot/launcher/bash/br"
-
-# Just (Task Runner)
-[[ -f "$BDOTDIR/bash_just.sh" ]] && source "$BDOTDIR/bash_just.sh"
-
-# Navi (Cheat Sheet Navigator)
-[[ -f "$BDOTDIR/bash_navi.sh" ]] && source "$BDOTDIR/bash_navi.sh"
-
-# Zoxide (Smart Directory Jumper)
 command -v zoxide >/dev/null && eval "$(zoxide init bash)"
 
 ####################################################################################################
